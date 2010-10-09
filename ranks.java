590c8f06c869f224d3ad697aa4a35f19f1cf3e0a
nossr50 <nossr50@gmail.com>
2010-10-08 18:38:42 -0700

    This is used with /promote and /demote, forgot to commit

---
+++ ranks.java
@@ -0,0 +1,9 @@
+/* Class of all ranks so I don't need to type them in */
+
+public class ranks {
+    public static final String[] Def = {"default"};
+    public static final String[] Trusted = {"trusted"};
+    public static final String[] Mods = {"mods"};
+    public static final String[] Admins = {"admins"};
+    public static final String[] SuperAdmins = {"superadmins"};
+}
