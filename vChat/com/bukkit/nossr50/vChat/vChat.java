9dcfa49b45a2bced076d844deb1bf71df2ad83d2
nossr50 <nossr50@gmail.com>
2011-01-16 00:21:38 -0800

    vMinecraft chat functionality ported to vChat

---
+++ vChat/com/bukkit/nossr50/vChat/vChat.java
@@ -0,0 +1,41 @@
+package com.bukkit.nossr50.vChat;
+
+import java.io.File;
+import java.util.HashMap;
+import org.bukkit.entity.Player;
+import org.bukkit.Server;
+import org.bukkit.event.Event.Priority;
+import org.bukkit.event.Event;
+import org.bukkit.plugin.PluginDescriptionFile;
+import org.bukkit.plugin.PluginLoader;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.plugin.PluginManager;
+
+/**
+ * vChat for Bukkit
+ *
+ * @author nossr50
+ * @author cerevisae
+ */
+public class vChat extends JavaPlugin {
+    private final vPlayerListener playerListener = new vPlayerListener(this);
+    private final HashMap<Player, Boolean> debugees = new HashMap<Player, Boolean>();
+
+    public vChat(PluginLoader pluginLoader, Server instance, PluginDescriptionFile desc, File folder, File plugin, ClassLoader cLoader) {
+        super(pluginLoader, instance, desc, folder, plugin, cLoader);
+    }
+
+   
+
+    public void onEnable() {
+        PluginManager pm = getServer().getPluginManager();
+        pm.registerEvent(Event.Type.PLAYER_CHAT, playerListener, Priority.Normal, this);
+        PluginDescriptionFile pdfFile = this.getDescription();
+        System.out.println( pdfFile.getName() + " version " + pdfFile.getVersion() + " is enabled!" );
+    }
+    public void onDisable() {
+        System.out.println("vChat Disabled!");
+    }
+}
+
+
