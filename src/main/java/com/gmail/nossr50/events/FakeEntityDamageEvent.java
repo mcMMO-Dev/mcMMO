04e2a212d160c19b59c4851c1cc3f1fb63b09614
nossr50 <nossr50@gmail.com>
2012-03-02 16:55:54 -0800

    Silly git

---
+++ src/main/java/com/gmail/nossr50/events/FakeEntityDamageEvent.java
@@ -0,0 +1,10 @@
+package com.gmail.nossr50.events;
+
+import org.bukkit.entity.Entity;
+import org.bukkit.event.entity.EntityDamageEvent;
+
+public class FakeEntityDamageEvent extends EntityDamageEvent{
+    public FakeEntityDamageEvent(Entity damagee, DamageCause cause, int damage) {
+        super(damagee, cause, damage);
+    }
+}
