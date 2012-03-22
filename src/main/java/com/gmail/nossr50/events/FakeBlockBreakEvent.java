b55e9c5c0daed9ba29f84daa981a2a4791d759b9
GJ <gjmcferrin@gmail.com>
2012-03-21 21:06:28 -0400

    Updated FakeBlockBreakEvent to align with recent Bukkit changes.

--- src/main/java/com/gmail/nossr50/events/FakeBlockBreakEvent.java
+++ src/main/java/com/gmail/nossr50/events/FakeBlockBreakEvent.java
@@ -1,12 +1,15 @@
 package com.gmail.nossr50.events;
 
+import java.util.ArrayList;
+
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.inventory.ItemStack;
 
 public class FakeBlockBreakEvent extends BlockBreakEvent {
 
 	public FakeBlockBreakEvent(Block theBlock, Player player) {
-		super(theBlock, player);
+		super(theBlock, player, new ArrayList<ItemStack>(theBlock.getDrops()));
 	}
 }
