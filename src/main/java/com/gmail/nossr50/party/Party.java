70ce762b5a01ed1445eda802ad90a06188aa5c27
nossr50 <nossr50@gmail.com>
2012-04-03 08:51:34 -0700

    Removing unused imports

--- src/main/java/com/gmail/nossr50/party/Party.java
+++ src/main/java/com/gmail/nossr50/party/Party.java
@@ -10,8 +10,6 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Iterator;
-
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
