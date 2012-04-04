d9b4647cf5b277ae33d20e4e78ca67e1712f1ec7
GJ <gjmcferrin@gmail.com>
2012-04-04 11:04:09 -0400

    Command updates

--- src/main/java/com/gmail/nossr50/commands/general/MmoupdateCommand.java
+++ src/main/java/com/gmail/nossr50/commands/general/MmoupdateCommand.java
@@ -8,37 +8,36 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 import com.gmail.nossr50.Users;
-import com.gmail.nossr50.mcPermissions;
+import com.gmail.nossr50.commands.CommandHelper;
 import com.gmail.nossr50.config.LoadProperties;
-import com.gmail.nossr50.locale.mcLocale;
 import com.gmail.nossr50.runnables.SQLConversionTask;
 
 public class MmoupdateCommand implements CommandExecutor {
-	@Override
-	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-		if (!(sender instanceof Player)) {
-			sender.sendMessage("This command does not support console useage."); //TODO: Needs more locale.
-			return true;
-		}
 
-		Player player = (Player) sender;
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (CommandHelper.noConsoleUsage(sender)) {
+            return true;
+        }
 
-		if (!mcPermissions.getInstance().admin(player)) {
-			player.sendMessage(ChatColor.YELLOW + "[mcMMO] " + ChatColor.DARK_RED + mcLocale.getString("mcPlayerListener.NoPermission"));
-			return true;
-		}
-		player.sendMessage(ChatColor.GRAY + "Starting conversion..."); //TODO: Needs more locale.
-		Users.clearUsers();
-		convertToMySQL();
-		for (Player x : Bukkit.getServer().getOnlinePlayers()) {
-			Users.addUser(x);
-		}
-		player.sendMessage(ChatColor.GREEN + "Conversion finished!"); //TODO: Needs more locale.
+        if (CommandHelper.noCommandPermissions(sender, "mcmmo.admin")) {
+            return true;
+        }
 
-		return true;
-	}
-	
-	/**
+        sender.sendMessage(ChatColor.GRAY + "Starting conversion..."); //TODO: Needs more locale.
+        Users.clearUsers();
+        convertToMySQL();
+
+        for (Player x : Bukkit.getServer().getOnlinePlayers()) {
+            Users.addUser(x);
+        }
+
+        sender.sendMessage(ChatColor.GREEN + "Conversion finished!"); //TODO: Needs more locale.
+
+        return true;
+    }
+
+    /**
      * Convert FlatFile data to MySQL data.
      */
     private void convertToMySQL() {
