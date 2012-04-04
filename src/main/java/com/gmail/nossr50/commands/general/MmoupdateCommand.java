2b7f21fee43123ceff20acb4893dfb80f75f800c
GJ <gjmcferrin@gmail.com>
2012-04-04 11:17:56 -0400

    Fixed nossr50's crappy merge. :D

--- src/main/java/com/gmail/nossr50/commands/general/MmoupdateCommand.java
+++ src/main/java/com/gmail/nossr50/commands/general/MmoupdateCommand.java
@@ -13,40 +13,9 @@ import com.gmail.nossr50.config.LoadProperties;
 import com.gmail.nossr50.runnables.SQLConversionTask;
 
 public class MmoupdateCommand implements CommandExecutor {
-<<<<<<< HEAD
-	@Override
-	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-	    Player player = null;
-	    
-	    if(sender instanceof Player) {
-	        player = (Player)sender;
-	    }
-	    
-		if (sender instanceof Player && !mcPermissions.getInstance().admin(player)) {
-			sender.sendMessage(ChatColor.YELLOW + "[mcMMO] " + ChatColor.DARK_RED + mcLocale.getString("mcPlayerListener.NoPermission"));
-			return true;
-		}
-		
-		sender.sendMessage(ChatColor.GRAY + "Starting conversion..."); //TODO: Needs more locale.
-		Users.clearUsers();
-		convertToMySQL();
-		for (Player x : Bukkit.getServer().getOnlinePlayers()) {
-			Users.addUser(x);
-		}
-		sender.sendMessage(ChatColor.GREEN + "Conversion finished!"); //TODO: Needs more locale.
-
-		return true;
-	}
-	
-	/**
-=======
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if (CommandHelper.noConsoleUsage(sender)) {
-            return true;
-        }
-
         if (CommandHelper.noCommandPermissions(sender, "mcmmo.admin")) {
             return true;
         }
@@ -65,7 +34,6 @@ public class MmoupdateCommand implements CommandExecutor {
     }
 
     /**
->>>>>>> d9b4647cf5b277ae33d20e4e78ca67e1712f1ec7
      * Convert FlatFile data to MySQL data.
      */
     private void convertToMySQL() {
