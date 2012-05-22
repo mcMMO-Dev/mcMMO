437a608d2b814554f200a919e50fc2bbc375aa61
NuclearW <incongruency@gmail.com>
2012-05-22 04:37:41 -0400

    Return on this error

--- src/main/java/com/gmail/nossr50/skills/repair/SimpleRepairManager.java
+++ src/main/java/com/gmail/nossr50/skills/repair/SimpleRepairManager.java
@@ -117,6 +117,7 @@ public class SimpleRepairManager implements RepairManager {
         // This should never happen, but if it does we need to complain loudly about it.
         if(repairItemLocation == -1) {
             player.sendMessage("mcMMO encountered an error attempting to repair this item!");  // TODO: Locale ?
+            return;
         }
 
         // Call event
