20b169e8eb9662a845663931ce206bbc020c29fc
NuclearW <incongruency@gmail.com>
2012-05-21 23:00:35 -0400

    Typo

--- src/main/java/com/gmail/nossr50/skills/repair/RepairableFactory.java
+++ src/main/java/com/gmail/nossr50/skills/repair/RepairableFactory.java
@@ -2,7 +2,7 @@ package com.gmail.nossr50.skills.repair;
 
 public class RepairableFactory {
     public static Repairable getRepairable(int itemId, int repairMaterialId, byte repairMetadata, int minimumLevel, int minimumQuantity, short maximumDurability, RepairItemType repairItemType, RepairMaterialType repairMaterialType, double xpMultiplier) {
-        // TODO: Add in loading from config what type of manager we want.
+        // TODO: Add in loading from config what type of repairable we want.
         return new SimpleRepairable(itemId, repairMaterialId, repairMetadata, minimumLevel, minimumQuantity, maximumDurability, repairItemType, repairMaterialType, xpMultiplier);
     }
 }
