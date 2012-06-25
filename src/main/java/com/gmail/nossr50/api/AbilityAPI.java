2559ea61d833882bc578b2fda1e277e6de3e1f00
GJ <gjmcferrin@gmail.com>
2012-06-25 10:05:47 -0400

    Added Ability API

---
+++ src/main/java/com/gmail/nossr50/api/AbilityAPI.java
@@ -0,0 +1,37 @@
+package com.gmail.nossr50.api;
+
+import org.bukkit.entity.Player;
+
+import com.gmail.nossr50.datatypes.AbilityType;
+import com.gmail.nossr50.util.Users;
+
+public class AbilityAPI {
+
+    public static boolean berserkEnabled(Player player) {
+        return Users.getProfile(player).getAbilityMode(AbilityType.BERSERK);
+    }
+
+    public static boolean gigaDrillBreakerEnabled(Player player) {
+        return Users.getProfile(player).getAbilityMode(AbilityType.GIGA_DRILL_BREAKER);
+    }
+
+    public static boolean greenTerraEnabled(Player player) {
+        return Users.getProfile(player).getAbilityMode(AbilityType.GREEN_TERRA);
+    }
+
+    public static boolean serratedStrikesEnabled(Player player) {
+        return Users.getProfile(player).getAbilityMode(AbilityType.SERRATED_STRIKES);
+    }
+
+    public static boolean skullSplitterEnabled(Player player) {
+        return Users.getProfile(player).getAbilityMode(AbilityType.SKULL_SPLIITER);
+    }
+
+    public static boolean superBreakerEnabled(Player player) {
+        return Users.getProfile(player).getAbilityMode(AbilityType.SUPER_BREAKER);
+    }
+
+    public static boolean treeFellerEnabled(Player player) {
+        return Users.getProfile(player).getAbilityMode(AbilityType.TREE_FELLER);
+    }
+}
