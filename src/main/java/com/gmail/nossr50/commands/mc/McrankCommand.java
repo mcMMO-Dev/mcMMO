1a888d3116be778b19923ad623202d5058d5f976
GJ <gjmcferrin@gmail.com>
2013-01-16 17:37:35 -0500

    Formatting, and reminding myself to come back and fix this even further.

--- src/main/java/com/gmail/nossr50/commands/mc/McrankCommand.java
+++ src/main/java/com/gmail/nossr50/commands/mc/McrankCommand.java
@@ -6,6 +6,7 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
+import com.gmail.nossr50.commands.CommandHelper;
 import com.gmail.nossr50.config.Config;
 import com.gmail.nossr50.datatypes.SkillType;
 import com.gmail.nossr50.locale.LocaleLoader;
@@ -17,30 +18,36 @@ public class McrankCommand implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-    	if(!Config.getInstance().getUseMySQL())
-    		Leaderboard.updateLeaderboards(); //Make sure the information is up to date
+        //TODO: Better input handling, add usage string
 
-        if(sender instanceof Player) {
-            Player player = (Player) sender;
-            String playerName;
-            switch(args.length){
-                case 0:
-                    playerName = player.getName();
-                    break;
-                case 1:
-                    playerName = args[0];
-                    break;
-                default:
-                    return false;
-            }
-            if(Config.getInstance().getUseMySQL()) {
-                sqlDisplay(sender, playerName);
-            } else {
-                flatfileDisplay(sender, playerName);
-            }
+        if (!Config.getInstance().getUseMySQL()) {
+            Leaderboard.updateLeaderboards(); //Make sure the information is up to date
+        }
 
-        } else {
-            sender.sendMessage("Command currently not supported for console.");
+        if (CommandHelper.noConsoleUsage(sender)) {
+            return true;
+        }
+
+        Player player = (Player) sender;
+        String playerName;
+        switch (args.length) {
+            case 0:
+                playerName = player.getName();
+                break;
+
+            case 1:
+                playerName = args[0];
+                break;
+
+            default:
+                return false;
+        }
+
+        if (Config.getInstance().getUseMySQL()) {
+            sqlDisplay(sender, playerName);
+        }
+        else {
+            flatfileDisplay(sender, playerName);
         }
 
         return true;
@@ -50,15 +57,16 @@ public class McrankCommand implements CommandExecutor {
         sender.sendMessage(LocaleLoader.getString("Commands.mcrank.Heading"));
         sender.sendMessage(LocaleLoader.getString("Commands.mcrank.Player", new Object[] {playerName}));
         for (SkillType skillType : SkillType.values()) {
-
             int[] rankInts = Leaderboard.getPlayerRank(playerName, skillType);
 
-            if (skillType.equals(SkillType.ALL))
+            if (skillType.equals(SkillType.ALL)) {
                 continue; // We want the overall ranking to be at the bottom
+            }
 
             if (rankInts[1] == 0) {
                 sender.sendMessage(LocaleLoader.getString("Commands.mcrank.Skill", new Object[] {Skills.localizeSkillName(skillType), LocaleLoader.getString("Commands.mcrank.Unranked")} )); //Don't bother showing ranking for players without skills
-            } else {
+            }
+            else {
                 sender.sendMessage(LocaleLoader.getString("Commands.mcrank.Skill", new Object[] {Skills.localizeSkillName(skillType), String.valueOf(rankInts[0])} ));
             }
         }
@@ -68,11 +76,11 @@ public class McrankCommand implements CommandExecutor {
         
         if (rankInts[1] == 0) {
             sender.sendMessage(LocaleLoader.getString("Commands.mcrank.Overalll", new Object[] {LocaleLoader.getString("Commands.mcrank.Unranked")} )); //Don't bother showing ranking for players without skills
-        } else {
+        }
+        else {
             sender.sendMessage(LocaleLoader.getString("Commands.mcrank.Overall", new Object[] {String.valueOf(rankInts[0])} ));
         }
     }
-
 
     private void sqlDisplay(CommandSender sender, String playerName) {
         Bukkit.getScheduler().runTaskAsynchronously(Bukkit.getPluginManager().getPlugin("mcMMO"), new McRankAsync(playerName, sender));
