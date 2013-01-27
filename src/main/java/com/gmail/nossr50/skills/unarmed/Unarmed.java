e061acb44a2e1d3557da2cdb99086a779b9d3bad
TfT_02 <tft_02@hotmail.com>
2013-01-27 22:49:10 +0100

    Fixing a bug where deflect and disarm would have wrong values.
    Adresses issue #593

--- src/main/java/com/gmail/nossr50/skills/unarmed/Unarmed.java
+++ src/main/java/com/gmail/nossr50/skills/unarmed/Unarmed.java
@@ -7,11 +7,11 @@ public class Unarmed {
     public static int ironArmMaxBonusDamage = AdvancedConfig.getInstance().getIronArmMaxBonus();
     public static int ironArmIncreaseLevel = AdvancedConfig.getInstance().getIronArmIncreaseLevel();
 
-    public static double deflectMaxChance = AdvancedConfig.getInstance().getDisarmChanceMax() ;
-    public static int deflectMaxBonusLevel = AdvancedConfig.getInstance().getDisarmMaxBonusLevel();
+    public static double disarmMaxChance = AdvancedConfig.getInstance().getDisarmChanceMax() ;
+    public static int disarmMaxBonusLevel = AdvancedConfig.getInstance().getDisarmMaxBonusLevel();
 
-    public static double disarmMaxChance = AdvancedConfig.getInstance().getDeflectChanceMax();
-    public static int disarmMaxBonusLevel = AdvancedConfig.getInstance().getDeflectMaxBonusLevel();
+    public static double deflectMaxChance = AdvancedConfig.getInstance().getDeflectChanceMax();
+    public static int deflectMaxBonusLevel = AdvancedConfig.getInstance().getDeflectMaxBonusLevel();
 
     public static double ironGripMaxChance = AdvancedConfig.getInstance().getIronGripChanceMax();
     public static int ironGripMaxBonusLevel = AdvancedConfig.getInstance().getIronGripMaxBonusLevel();
