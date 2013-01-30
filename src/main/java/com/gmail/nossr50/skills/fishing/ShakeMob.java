ae49f0b6e4645460fb7b33069650e6e6a6e6c682
bm01 <baptiste.marie@orange.fr>
2013-01-30 01:15:07 +0100

    Remove unused import Whoops D:

--- src/main/java/com/gmail/nossr50/skills/fishing/ShakeMob.java
+++ src/main/java/com/gmail/nossr50/skills/fishing/ShakeMob.java
@@ -4,7 +4,6 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Map.Entry;
 
-import org.bukkit.DyeColor;
 import org.bukkit.Material;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
