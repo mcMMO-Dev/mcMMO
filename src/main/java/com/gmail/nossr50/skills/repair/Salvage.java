761fb3b8113fb11050aa0bc69b65eb7ffd065f0b
bm01 <baptiste.marie@orange.fr>
2013-02-12 19:19:13 +0100

    Bad copy paste

--- src/main/java/com/gmail/nossr50/skills/repair/Salvage.java
+++ src/main/java/com/gmail/nossr50/skills/repair/Salvage.java
@@ -141,7 +141,7 @@ public class Salvage {
      * @return true if the item is salvageable, false otherwise
      */
     public static boolean isSalvageable(final ItemStack is) {
-        if (configInstance.getSalvageTools() && (ItemChecks.isMinecraftArmor(is) || ItemChecks.isStringTool(is) || is.getType() == Material.BUCKET)) {
+        if (configInstance.getSalvageTools() && (ItemChecks.isMinecraftTool(is) || ItemChecks.isStringTool(is) || is.getType() == Material.BUCKET)) {
             return true;
         }
         if (configInstance.getSalvageArmor() && ItemChecks.isMinecraftArmor(is)) {
