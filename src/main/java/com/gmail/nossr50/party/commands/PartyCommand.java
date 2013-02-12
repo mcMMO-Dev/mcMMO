5a48d5c5f84d24a2cedfbfeb221784f1a33d4236
GJ <gjmcferrin@gmail.com>
2013-02-12 10:42:42 -0500

    Convert /party info to a subcommand

--- src/main/java/com/gmail/nossr50/party/commands/PartyCommand.java
+++ src/main/java/com/gmail/nossr50/party/commands/PartyCommand.java
@@ -33,6 +33,7 @@ public class PartyCommand implements CommandExecutor {
     private CommandExecutor partyLockCommand = new PartyLockCommand();
     private CommandExecutor partyChangePasswordCommand = new PartyChangePasswordCommand();
     private CommandExecutor partyRenameCommand = new PartyRenameCommand();
+    private CommandExecutor partyInfoCommand = new PartyInfoCommand();
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
@@ -49,10 +50,9 @@ public class PartyCommand implements CommandExecutor {
         mcMMOPlayer = Users.getPlayer(player);
 
         if (args.length < 1 || args[0].equalsIgnoreCase("info")) {
-            return party();
+            return partyInfoCommand.onCommand(sender, command, label, args);
         }
-
-        if (args[0].equalsIgnoreCase("join")) {
+        else if (args[0].equalsIgnoreCase("join")) {
             return partyJoinCommand.onCommand(sender, command, label, args);
         }
         else if (args[0].equalsIgnoreCase("accept")) {
@@ -115,71 +115,6 @@ public class PartyCommand implements CommandExecutor {
         player.sendMessage(LocaleLoader.getString("Party.Help.0"));
         player.sendMessage(LocaleLoader.getString("Party.Help.1"));
         player.sendMessage(LocaleLoader.getString("Party.Help.2"));
-        return true;
-    }
-
-    private boolean party() {
-        if (mcMMOPlayer.inParty()) {
-            Party party = mcMMOPlayer.getParty();
-            String leader = party.getLeader();
-            StringBuilder tempList = new StringBuilder();
-
-            int membersNear = PartyManager.getNearMembers(player, party, Config.getInstance().getPartyShareRange()).size();
-            int membersOnline = party.getOnlineMembers().size() - 1;
-
-            String ItemShare = "";
-            String ExpShare = "";
-            String Split = "";
-
-            for (OfflinePlayer otherMember : party.getMembers()) {
-                if (leader.equals(otherMember.getName())) {
-                    tempList.append(ChatColor.GOLD);
-                }
-                else if (otherMember.isOnline()) {
-                    tempList.append(ChatColor.WHITE);
-                }
-                else {
-                    tempList.append(ChatColor.GRAY);
-                }
-
-                tempList.append(otherMember.getName()).append(" ");
-            }
-
-            String status = LocaleLoader.getString("Party.Status.Locked");
-            if (!party.isLocked()) {
-                status = LocaleLoader.getString("Party.Status.Unlocked");
-            }
-
-            player.sendMessage(LocaleLoader.getString("Commands.Party.Header"));
-            player.sendMessage(LocaleLoader.getString("Commands.Party.Status", party.getName(), status));
-
-            boolean xpShareEnabled = Config.getInstance().getExpShareEnabled();
-            boolean itemShareEnabled = Config.getInstance().getItemShareEnabled();
-
-            if (xpShareEnabled) {
-                ExpShare = LocaleLoader.getString("Commands.Party.ExpShare", party.getXpShareMode().toString());
-            }
-
-            if (itemShareEnabled) {
-                ItemShare = LocaleLoader.getString("Commands.Party.ItemShare", party.getItemShareMode().toString());
-            }
-
-            if (xpShareEnabled && itemShareEnabled) {
-                Split = ChatColor.DARK_GRAY + " || ";
-            }
-
-            if (xpShareEnabled || itemShareEnabled) {
-                player.sendMessage(LocaleLoader.getString("Commands.Party.ShareMode") + ExpShare + Split + ItemShare);
-            }
-
-            player.sendMessage(LocaleLoader.getString("Commands.Party.Members.Header"));
-            player.sendMessage(LocaleLoader.getString("Commands.Party.MembersNear", membersNear, membersOnline));
-            player.sendMessage(LocaleLoader.getString("Commands.Party.Members", tempList));
-        }
-        else {
-            return printUsage();
-        }
-
         return true;
     }
 
