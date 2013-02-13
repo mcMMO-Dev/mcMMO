7350fdb99cee101c18aa8b947b6901333d999335
GJ <gjmcferrin@gmail.com>
2013-02-12 23:21:34 -0500

    Remove unused import.

--- src/main/java/com/gmail/nossr50/commands/admin/XprateCommand.java
+++ src/main/java/com/gmail/nossr50/commands/admin/XprateCommand.java
@@ -3,7 +3,6 @@ package com.gmail.nossr50.commands.admin;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
 
 import com.gmail.nossr50.mcMMO;
 import com.gmail.nossr50.config.Config;
