cd4c32832083a0cf79ea9afa7e0ad23741ea6431
bm01 <baptiste.marie@orange.fr>
2013-03-08 02:03:25 +0100

    Improved SkillMonitorTask

--- src/main/java/com/gmail/nossr50/runnables/skills/SkillMonitorTask.java
+++ src/main/java/com/gmail/nossr50/runnables/skills/SkillMonitorTask.java
@@ -1,12 +1,8 @@
 package com.gmail.nossr50.runnables.skills;
 
-import org.bukkit.entity.Player;
-
-import com.gmail.nossr50.mcMMO;
 import com.gmail.nossr50.datatypes.player.McMMOPlayer;
 import com.gmail.nossr50.datatypes.skills.AbilityType;
 import com.gmail.nossr50.datatypes.skills.SkillType;
-import com.gmail.nossr50.util.Misc;
 import com.gmail.nossr50.util.player.UserManager;
 import com.gmail.nossr50.util.skills.SkillUtils;
 
@@ -15,13 +11,7 @@ public class SkillMonitorTask implements Runnable {
     public void run() {
         long curTime = System.currentTimeMillis();
 
-        for (Player player : mcMMO.p.getServer().getOnlinePlayers()) {
-            if (Misc.isNPCEntity(player)) {
-                continue;
-            }
-
-            McMMOPlayer mcMMOPlayer = UserManager.getPlayer(player);
-
+        for (McMMOPlayer mcMMOPlayer : UserManager.getPlayers().values()) {
             /*
              * MONITOR SKILLS
              */
