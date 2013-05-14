0b18ae8ff2b615214ff692fd655f428e1d23f483
T00thpick1 <t00thpick1dirko@gmail.com>
2013-05-13 22:26:47 -0400

    Formatting and things

--- src/main/java/com/gmail/nossr50/events/skills/abilities/McMMOPlayerAbilityActivateEvent.java
+++ src/main/java/com/gmail/nossr50/events/skills/abilities/McMMOPlayerAbilityActivateEvent.java
@@ -1,12 +1,13 @@
 package com.gmail.nossr50.events.skills.abilities;
 
 import org.bukkit.entity.Player;
+import org.bukkit.event.Cancellable;
 
 import com.gmail.nossr50.datatypes.skills.AbilityType;
 import com.gmail.nossr50.datatypes.skills.SkillType;
 import com.gmail.nossr50.events.skills.McMMOPlayerSkillEvent;
 
-public class McMMOPlayerAbilityActivateEvent extends McMMOPlayerSkillEvent {
+public class McMMOPlayerAbilityActivateEvent extends McMMOPlayerSkillEvent implements Cancellable {
 
     private AbilityType abilityType;
     private boolean cancelled;
@@ -16,7 +17,10 @@ public class McMMOPlayerAbilityActivateEvent extends McMMOPlayerSkillEvent {
         abilityType = skill.getAbility();
         cancelled = false;
     }
-    
+
+    /**
+     * @return The ability type involved in this event
+     */
     public AbilityType getAbilityType() {
         return abilityType;
     }
@@ -24,9 +28,8 @@ public class McMMOPlayerAbilityActivateEvent extends McMMOPlayerSkillEvent {
     public boolean isCancelled() {
         return cancelled;
     }
-    
+
     public void setCancelled(boolean value) {
         this.cancelled = value;
     }
-
 }
