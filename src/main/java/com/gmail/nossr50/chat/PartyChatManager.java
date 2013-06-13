602956a555622fcbf9d764f74a1f610b26c555e3
GJ <gjmcferrin@gmail.com>
2013-06-13 11:10:46 -0400

    Fix duplicate prefix sending with party chat & logging.

--- src/main/java/com/gmail/nossr50/chat/PartyChatManager.java
+++ src/main/java/com/gmail/nossr50/chat/PartyChatManager.java
@@ -1,13 +1,11 @@
 package com.gmail.nossr50.chat;
 
-import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 
 import com.gmail.nossr50.config.Config;
 import com.gmail.nossr50.datatypes.party.Party;
 import com.gmail.nossr50.events.chat.McMMOPartyChatEvent;
-import com.gmail.nossr50.locale.LocaleLoader;
 
 public class PartyChatManager extends ChatManager {
     private Party party;
@@ -28,9 +26,9 @@ public class PartyChatManager extends ChatManager {
     @Override
     protected void sendMessage() {
         for (Player member : party.getOnlineMembers()) {
-            member.sendMessage(LocaleLoader.getString("Commands.Party.Chat.Prefix", displayName) + message);
+            member.sendMessage(message);
         }
 
-        plugin.getLogger().info("[P](" + party.getName() + ")" + "<" + ChatColor.stripColor(displayName) + "> " + message);
+        plugin.getLogger().info("[P]<" + party.getName() + ">" + message);
     }
 }
