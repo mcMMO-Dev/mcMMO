9073aa6a6e673a7b6a8dee2753d3566d8b907e38
dddeeefff <dddeeefff@gmail.com>
2013-09-23 20:57:28 +0100

    Add random horse colour and style (pattern) generation to Call Of The Wild

--- src/main/java/com/gmail/nossr50/skills/taming/TamingManager.java
+++ src/main/java/com/gmail/nossr50/skills/taming/TamingManager.java
@@ -5,6 +5,7 @@ import org.bukkit.entity.EntityType;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Ocelot;
 import org.bukkit.entity.Player;
+import org.bukkit.entity.Horse;
 import org.bukkit.entity.Tameable;
 import org.bukkit.entity.Wolf;
 import org.bukkit.inventory.ItemStack;
@@ -230,6 +231,11 @@ public class TamingManager extends SkillManager {
                 case WOLF:
                     entity.setMaxHealth(20.0);
                     entity.setHealth(entity.getMaxHealth());
+                    break;
+
+                case HORSE:
+                    ((Horse) entity).setColor(Horse.Color.values()[0 + Misc.getRandom().nextInt(Horse.Color.values().length)]);
+                    ((Horse) entity).setStyle(Horse.Style.values()[0 + Misc.getRandom().nextInt(Horse.Style.values().length)]);
                     break;
 
                 default:
