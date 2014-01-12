491df2cc822044ca0778efc9911655ac54026c57
TfT_02 <tft_02@hotmail.com>
2014-01-12 13:51:12 +0100

    Changed the appearance of /mcmmo commands
    Closes #978

--- src/main/java/com/gmail/nossr50/commands/McmmoCommand.java
+++ src/main/java/com/gmail/nossr50/commands/McmmoCommand.java
@@ -42,9 +42,9 @@ public class McmmoCommand implements CommandExecutor {
                     }
 
                     sender.sendMessage(LocaleLoader.getString("Commands.mcc.Header"));
-                    displayPartyCommands(sender);
+                    displayGeneralCommands(sender);
                     displayOtherCommands(sender);
-
+                    displayPartyCommands(sender);
                 }
                 return true;
 
@@ -53,55 +53,57 @@ public class McmmoCommand implements CommandExecutor {
         }
     }
 
-    private void displayPartyCommands(CommandSender sender) {
-        if (Permissions.party(sender)) {
-            sender.sendMessage(LocaleLoader.getString("Commands.Party.Commands"));
-            sender.sendMessage(" /party create <" + LocaleLoader.getString("Commands.Usage.PartyName") + "> " + LocaleLoader.getString("Commands.Party1"));
-            sender.sendMessage(" /party join <" + LocaleLoader.getString("Commands.Usage.Player") + "> " + LocaleLoader.getString("Commands.Party2"));
-            sender.sendMessage(" /party quit " + LocaleLoader.getString("Commands.Party.Quit"));
+    private void displayGeneralCommands(CommandSender sender) {
+        sender.sendMessage(ChatColor.DARK_AQUA + " /mcstats " + LocaleLoader.getString("Commands.Stats"));
+        sender.sendMessage(ChatColor.DARK_AQUA + " /<skill>" + LocaleLoader.getString("Commands.SkillInfo"));
+        sender.sendMessage(ChatColor.DARK_AQUA + " /mctop " + LocaleLoader.getString("Commands.Leaderboards"));
 
-            if (Permissions.partyChat(sender)) {
-                sender.sendMessage(" /party chat " + LocaleLoader.getString("Commands.Party.Toggle"));
-            }
+        if (Permissions.inspect(sender)) {
+            sender.sendMessage(ChatColor.DARK_AQUA + " /inspect " + LocaleLoader.getString("Commands.Inspect"));
+        }
 
-            sender.sendMessage(" /party invite <" + LocaleLoader.getString("Commands.Usage.Player") + "> " + LocaleLoader.getString("Commands.Party.Invite"));
-            sender.sendMessage(" /party accept " + LocaleLoader.getString("Commands.Party.Accept"));
-
-            if (Permissions.partySubcommand(sender, PartySubcommandType.TELEPORT)) {
-                sender.sendMessage(" /party teleport " + LocaleLoader.getString("Commands.Party.Teleport"));
-            }
+        if (Permissions.mcability(sender)) {
+            sender.sendMessage(ChatColor.DARK_AQUA + " /mcability " + LocaleLoader.getString("Commands.ToggleAbility"));
         }
     }
 
     private void displayOtherCommands(CommandSender sender) {
         sender.sendMessage(LocaleLoader.getString("Commands.Other"));
-        sender.sendMessage(" /mcstats " + LocaleLoader.getString("Commands.Stats"));
-        sender.sendMessage(" /mctop " + LocaleLoader.getString("Commands.Leaderboards"));
 
         if (Permissions.skillreset(sender)) {
-            sender.sendMessage(" /skillreset <skill|all> " + LocaleLoader.getString("Commands.Reset"));
-        }
-
-        if (Permissions.mcability(sender)) {
-            sender.sendMessage(" /mcability " + LocaleLoader.getString("Commands.ToggleAbility"));
-        }
-
-        if (Permissions.adminChat(sender)) {
-            sender.sendMessage(" /adminchat " + LocaleLoader.getString("Commands.AdminToggle"));
-        }
-
-        if (Permissions.inspect(sender)) {
-            sender.sendMessage(" /inspect " + LocaleLoader.getString("Commands.Inspect"));
+            sender.sendMessage(ChatColor.DARK_AQUA + " /skillreset <skill|all> " + LocaleLoader.getString("Commands.Reset"));
         }
 
         if (Permissions.mmoedit(sender)) {
-            sender.sendMessage(" /mmoedit " + LocaleLoader.getString("Commands.mmoedit"));
+            sender.sendMessage(ChatColor.DARK_AQUA + " /mmoedit " + LocaleLoader.getString("Commands.mmoedit"));
+        }
+
+        if (Permissions.adminChat(sender)) {
+            sender.sendMessage(ChatColor.DARK_AQUA + " /adminchat " + LocaleLoader.getString("Commands.AdminToggle"));
         }
 
         if (Permissions.mcgod(sender)) {
-            sender.sendMessage(" /mcgod " + LocaleLoader.getString("Commands.mcgod"));
+            sender.sendMessage(ChatColor.DARK_AQUA + " /mcgod " + LocaleLoader.getString("Commands.mcgod"));
         }
+    }
 
-        sender.sendMessage(" /<skill>" + LocaleLoader.getString("Commands.SkillInfo"));
+    private void displayPartyCommands(CommandSender sender) {
+        if (Permissions.party(sender)) {
+            sender.sendMessage(LocaleLoader.getString("Commands.Party.Commands"));
+            sender.sendMessage(ChatColor.DARK_AQUA + " /party create <" + LocaleLoader.getString("Commands.Usage.PartyName") + "> " + LocaleLoader.getString("Commands.Party1"));
+            sender.sendMessage(ChatColor.DARK_AQUA + " /party join <" + LocaleLoader.getString("Commands.Usage.Player") + "> " + LocaleLoader.getString("Commands.Party2"));
+            sender.sendMessage(ChatColor.DARK_AQUA + " /party quit " + LocaleLoader.getString("Commands.Party.Quit"));
+
+            if (Permissions.partyChat(sender)) {
+                sender.sendMessage(ChatColor.DARK_AQUA + " /party chat " + LocaleLoader.getString("Commands.Party.Toggle"));
+            }
+
+            sender.sendMessage(ChatColor.DARK_AQUA + " /party invite <" + LocaleLoader.getString("Commands.Usage.Player") + "> " + LocaleLoader.getString("Commands.Party.Invite"));
+            sender.sendMessage(ChatColor.DARK_AQUA + " /party accept " + LocaleLoader.getString("Commands.Party.Accept"));
+
+            if (Permissions.partySubcommand(sender, PartySubcommandType.TELEPORT)) {
+                sender.sendMessage(ChatColor.DARK_AQUA + " /party teleport <" + LocaleLoader.getString("Commands.Usage.Player") + "> " + LocaleLoader.getString("Commands.Party.Teleport"));
+            }
+        }
     }
 }
