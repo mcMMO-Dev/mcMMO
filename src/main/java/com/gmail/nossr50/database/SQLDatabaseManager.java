9ee4f6316f4b693cea0067c15faacb0e0b77d03c
TfT_02 <tft_02@hotmail.com>
2014-04-14 14:14:52 +0200

    Add u.uuid here

--- src/main/java/com/gmail/nossr50/database/SQLDatabaseManager.java
+++ src/main/java/com/gmail/nossr50/database/SQLDatabaseManager.java
@@ -368,7 +368,7 @@ public final class SQLDatabaseManager implements DatabaseManager {
                     + "s.taming, s.mining, s.repair, s.woodcutting, s.unarmed, s.herbalism, s.excavation, s.archery, s.swords, s.axes, s.acrobatics, s.fishing, s.alchemy, "
                     + "e.taming, e.mining, e.repair, e.woodcutting, e.unarmed, e.herbalism, e.excavation, e.archery, e.swords, e.axes, e.acrobatics, e.fishing, e.alchemy, "
                     + "c.taming, c.mining, c.repair, c.woodcutting, c.unarmed, c.herbalism, c.excavation, c.archery, c.swords, c.axes, c.acrobatics, c.blast_mining, "
-                    + "h.mobhealthbar "
+                    + "h.mobhealthbar, u.uuid "
                     + "FROM " + tablePrefix + "users u "
                     + "JOIN " + tablePrefix + "skills s ON (u.id = s.user_id) "
                     + "JOIN " + tablePrefix + "experience e ON (u.id = e.user_id) "
