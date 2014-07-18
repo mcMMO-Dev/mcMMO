96fdf265d53f406ca8b84cb530293c001b98fff7
TfT_02 <tft_02@hotmail.com>
2014-07-18 17:06:30 +0200

    Don't run party chat on a separate thread
    Fixes #2080

--- src/main/java/com/gmail/nossr50/chat/PartyChatManager.java
+++ src/main/java/com/gmail/nossr50/chat/PartyChatManager.java
@@ -1,16 +1,11 @@
 package com.gmail.nossr50.chat;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
-import org.bukkit.ChatColor;
-import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 
 import com.gmail.nossr50.config.Config;
 import com.gmail.nossr50.datatypes.party.Party;
 import com.gmail.nossr50.events.chat.McMMOPartyChatEvent;
-import com.gmail.nossr50.locale.LocaleLoader;
+import com.gmail.nossr50.runnables.party.PartyChatTask;
 
 public class PartyChatManager extends ChatManager {
     private Party party;
@@ -30,21 +25,6 @@ public class PartyChatManager extends ChatManager {
 
     @Override
     protected void sendMessage() {
-        if (Config.getInstance().getPartyChatColorLeaderName() && senderName.equalsIgnoreCase(party.getLeader())) {
-            message = message.replaceFirst(Pattern.quote(displayName), ChatColor.GOLD + Matcher.quoteReplacement(displayName) + ChatColor.RESET);
-        }
-
-        for (Player member : party.getOnlineMembers()) {
-            member.sendMessage(message);
-        }
-
-        if (party.getAlly() != null) {
-            for (Player member : party.getAlly().getOnlineMembers()) {
-                String allyPrefix = LocaleLoader.formatString(Config.getInstance().getPartyChatPrefixAlly());
-                member.sendMessage(allyPrefix + message);
-            }
-        }
-
-        plugin.getServer().getConsoleSender().sendMessage("[mcMMO] [P]<" + party.getName() + ">" + message);
+        new PartyChatTask(plugin, party, senderName, displayName, message).runTask(plugin);
     }
 }
