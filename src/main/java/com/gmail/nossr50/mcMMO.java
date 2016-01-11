00e18d4096e88eb065d532802dcbd9d106534479
t00thpick1 <t00thpick1dirko@gmail.com>
2016-01-11 14:12:54 -0500

    Remove metrics

--- src/main/java/com/gmail/nossr50/mcMMO.java
+++ src/main/java/com/gmail/nossr50/mcMMO.java
@@ -31,7 +31,6 @@ import com.gmail.nossr50.listeners.InventoryListener;
 import com.gmail.nossr50.listeners.PlayerListener;
 import com.gmail.nossr50.listeners.SelfListener;
 import com.gmail.nossr50.listeners.WorldListener;
-import com.gmail.nossr50.metrics.MetricsManager;
 import com.gmail.nossr50.party.PartyManager;
 import com.gmail.nossr50.runnables.CheckDateTask;
 import com.gmail.nossr50.runnables.SaveTimerTask;
@@ -178,8 +177,6 @@ public class mcMMO extends JavaPlugin {
 
             scheduleTasks();
             CommandRegistrationManager.registerCommands();
-
-            MetricsManager.setup();
 
             placeStore = ChunkManagerFactory.getChunkManager(); // Get our ChunkletManager
 
