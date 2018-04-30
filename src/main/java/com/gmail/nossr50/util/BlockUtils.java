5b2ebf4aa866fa85335f237d5d401ff6ecd6e80b
Glitchfinder <glitchkey@gmail.com>
2018-04-29 23:09:52 -0700

    Prevent shulker boxes from activating abilities.
    Fixes #3438.

--- src/main/java/com/gmail/nossr50/util/BlockUtils.java
+++ src/main/java/com/gmail/nossr50/util/BlockUtils.java
@@ -88,7 +88,22 @@ public final class BlockUtils {
             case JUNGLE_FENCE :
             case SPRUCE_FENCE :
             case ARMOR_STAND :
-            case SHULKER_SHELL:
+            case BLACK_SHULKER_BOX :
+            case BLUE_SHULKER_BOX :
+            case BROWN_SHULKER_BOX :
+            case CYAN_SHULKER_BOX :
+            case GRAY_SHULKER_BOX :
+            case GREEN_SHULKER_BOX :
+            case LIGHT_BLUE_SHULKER_BOX :
+            case LIME_SHULKER_BOX :
+            case MAGENTA_SHULKER_BOX :
+            case ORANGE_SHULKER_BOX :
+            case PINK_SHULKER_BOX :
+            case PURPLE_SHULKER_BOX :
+            case RED_SHULKER_BOX :
+            case SILVER_SHULKER_BOX :
+            case WHITE_SHULKER_BOX :
+            case YELLOW_SHULKER_BOX :
                 return false;
 
             default :
