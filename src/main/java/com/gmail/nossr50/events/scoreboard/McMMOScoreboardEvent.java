7f40b4294f64d8757f2b20e1cd5c3fc3484ea9e1
nossr50 <nossr50@gmail.com>
2019-01-12 14:42:30 -0800

    mcMMO now fires many new custom events relating to scoreboards

---
+++ src/main/java/com/gmail/nossr50/events/scoreboard/McMMOScoreboardEvent.java
@@ -0,0 +1,4 @@
+package com.gmail.nossr50.events.scoreboard;
+
+public class McMMOScoreboardEvent {
+}
---
+++ src/main/java/com/gmail/nossr50/events/scoreboard/ScoreboardEventReason.java
@@ -0,0 +1,7 @@
+package com.gmail.nossr50.events.scoreboard;
+
+public enum ScoreboardEventReason {
+    CREATING_NEW_SCOREBOARD,
+    OBJECTIVE,
+    REVERTING_BOARD,
+}
