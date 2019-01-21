c728b233e66a9f682b11df93d7658d571852b677
nossr50 <nossr50@gmail.com>
2019-01-21 07:40:04 -0800

    Locale files now support &-code formatting used by many plugins

--- src/main/java/com/gmail/nossr50/locale/LocaleLoader.java
+++ src/main/java/com/gmail/nossr50/locale/LocaleLoader.java
@@ -114,6 +114,29 @@ public final class LocaleLoader {
         input = input.replaceAll("\\Q[[MAGIC]]\\E", ChatColor.MAGIC.toString());
         input = input.replaceAll("\\Q[[RESET]]\\E", ChatColor.RESET.toString());
 
+        input = input.replaceAll("\\Q&0\\E", ChatColor.BLACK.toString());
+        input = input.replaceAll("\\Q&1\\E", ChatColor.DARK_BLUE.toString());
+        input = input.replaceAll("\\Q&2\\E", ChatColor.DARK_GREEN.toString());
+        input = input.replaceAll("\\Q&3\\E", ChatColor.DARK_AQUA.toString());
+        input = input.replaceAll("\\Q&4\\E", ChatColor.DARK_RED.toString());
+        input = input.replaceAll("\\Q&5\\E", ChatColor.DARK_PURPLE.toString());
+        input = input.replaceAll("\\Q&6\\E", ChatColor.GOLD.toString());
+        input = input.replaceAll("\\Q&7\\E", ChatColor.GRAY.toString());
+        input = input.replaceAll("\\Q&8\\E", ChatColor.DARK_GRAY.toString());
+        input = input.replaceAll("\\Q&9\\E", ChatColor.BLUE.toString());
+        input = input.replaceAll("\\Q&a\\E", ChatColor.GREEN.toString());
+        input = input.replaceAll("\\Q&b\\E", ChatColor.AQUA.toString());
+        input = input.replaceAll("\\Q&c\\E", ChatColor.RED.toString());
+        input = input.replaceAll("\\Q&d\\E", ChatColor.LIGHT_PURPLE.toString());
+        input = input.replaceAll("\\Q&e\\E", ChatColor.YELLOW.toString());
+        input = input.replaceAll("\\Q&f\\E", ChatColor.WHITE.toString());
+        input = input.replaceAll("\\Q&l\\E", ChatColor.BOLD.toString());
+        input = input.replaceAll("\\Q&n\\E", ChatColor.UNDERLINE.toString());
+        input = input.replaceAll("\\Q&o\\E", ChatColor.ITALIC.toString());
+        input = input.replaceAll("\\Q&m\\E", ChatColor.STRIKETHROUGH.toString());
+        input = input.replaceAll("\\Q&?\\E", ChatColor.MAGIC.toString());
+        input = input.replaceAll("\\Q&r\\E", ChatColor.RESET.toString());
+
         return input;
     }
 }
