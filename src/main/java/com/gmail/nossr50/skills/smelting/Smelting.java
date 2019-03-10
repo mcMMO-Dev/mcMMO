7703cafa3403b014fc86bd59e5613ff529a0082b
nossr50 <nossr50@gmail.com>
2019-03-09 16:25:51 -0800

    fixing compile error

--- src/main/java/com/gmail/nossr50/skills/smelting/Smelting.java
+++ src/main/java/com/gmail/nossr50/skills/smelting/Smelting.java
@@ -16,11 +16,7 @@ public class Smelting {
         return RankUtils.getRank(player, SubSkillType.SMELTING_UNDERSTANDING_THE_ART);
     }
 
-    public static int    burnModifierMaxLevel = AdvancedConfig.getInstance().getBurnModifierMaxLevel();
-    public static double burnTimeMultiplier   = AdvancedConfig.getInstance().getBurnTimeMultiplier();
-
     public static int    fluxMiningUnlockLevel = RankUtils.getUnlockLevel(SubSkillType.SMELTING_FLUX_MINING);
-    public static double fluxMiningChance      = AdvancedConfig.getInstance().getFluxMiningChance();
 
     protected static int getResourceXp(ItemStack smelting) {
         return mcMMO.getModManager().isCustomOre(smelting.getType()) ? mcMMO.getModManager().getBlock(smelting.getType()).getSmeltingXpGain() : ExperienceConfig.getInstance().getXp(PrimarySkillType.SMELTING, smelting.getType());
