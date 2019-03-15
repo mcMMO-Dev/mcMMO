4c78afc04b519e7b951ff90a8570513ff9154256
nossr50 <nossr50@gmail.com>
2019-03-14 20:28:04 -0700

    Add party config

--- src/main/java/com/gmail/nossr50/config/hocon/party/ConfigParty.java
+++ src/main/java/com/gmail/nossr50/config/hocon/party/ConfigParty.java
@@ -1,7 +1,7 @@
-package com.gmail.nossr50.config.hocon;
+package com.gmail.nossr50.config.hocon.party;
 
 import ninja.leaping.configurate.objectmapping.serialize.ConfigSerializable;
 
 @ConfigSerializable
-public class Particles {
+public class ConfigParty {
 }
