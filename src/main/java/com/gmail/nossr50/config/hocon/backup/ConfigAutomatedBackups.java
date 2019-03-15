0bed8898bf1cbba53812ad4d1916124790df5a65
nossr50 <nossr50@gmail.com>
2019-03-14 20:28:18 -0700

    Add automated backup config

--- src/main/java/com/gmail/nossr50/config/hocon/backup/ConfigAutomatedBackups.java
+++ src/main/java/com/gmail/nossr50/config/hocon/backup/ConfigAutomatedBackups.java
@@ -1,7 +1,7 @@
-package com.gmail.nossr50.config.hocon;
+package com.gmail.nossr50.config.hocon.backup;
 
 import ninja.leaping.configurate.objectmapping.serialize.ConfigSerializable;
 
 @ConfigSerializable
-public class PlayerNotifications {
+public class ConfigAutomatedBackups {
 }
