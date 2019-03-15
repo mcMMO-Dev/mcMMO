cb21c0bf2481cab41722bfed6a29958adb7bca47
nossr50 <nossr50@gmail.com>
2019-03-14 20:27:57 -0700

    Add particles config

---
+++ src/main/java/com/gmail/nossr50/config/hocon/particles/ConfigParticles.java
@@ -0,0 +1,7 @@
+package com.gmail.nossr50.config.hocon.particles;
+
+import ninja.leaping.configurate.objectmapping.serialize.ConfigSerializable;
+
+@ConfigSerializable
+public class ConfigParticles {
+}
