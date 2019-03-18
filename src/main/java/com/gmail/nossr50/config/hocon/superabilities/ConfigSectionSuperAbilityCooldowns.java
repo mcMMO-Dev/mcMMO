7d2e76e464fb1eef7d3567ac9257515dbea819fb
nossr50 <nossr50@gmail.com>
2019-03-18 09:16:11 -0700

    Super Ability config pt 2

---
+++ src/main/java/com/gmail/nossr50/config/hocon/superabilities/ConfigSectionSuperAbilityCooldowns.java
@@ -0,0 +1,64 @@
+package com.gmail.nossr50.config.hocon.superabilities;
+
+import ninja.leaping.configurate.objectmapping.Setting;
+import ninja.leaping.configurate.objectmapping.serialize.ConfigSerializable;
+
+@ConfigSerializable
+public class ConfigSectionSuperAbilityCooldowns {
+
+    @Setting(value = "Berserk")
+    private int berserk = 240;
+
+    @Setting(value = "Giga-Drill-Breaker")
+    private int gigaDrillBreaker = 240;
+
+    @Setting(value = "Green-Terra")
+    private int greenTerra = 240;
+
+    @Setting(value = "Serrated-Strikes")
+    private int serratedStrikes = 240;
+
+    @Setting(value = "Skull-Splitter")
+    private int skullSplitter = 240;
+
+    @Setting(value = "Super-Breaker")
+    private int superBreaker = 240;
+
+    @Setting(value = "Tree-Feller")
+    private int treeFeller = 240;
+
+    @Setting(value = "Blast-Mining")
+    private int blastMining = 120;
+
+    public int getBerserk() {
+        return berserk;
+    }
+
+    public int getGigaDrillBreaker() {
+        return gigaDrillBreaker;
+    }
+
+    public int getGreenTerra() {
+        return greenTerra;
+    }
+
+    public int getSerratedStrikes() {
+        return serratedStrikes;
+    }
+
+    public int getSkullSplitter() {
+        return skullSplitter;
+    }
+
+    public int getSuperBreaker() {
+        return superBreaker;
+    }
+
+    public int getTreeFeller() {
+        return treeFeller;
+    }
+
+    public int getBlastMining() {
+        return blastMining;
+    }
+}
--- src/main/java/com/gmail/nossr50/config/hocon/superabilities/ConfigSuperAbilities.java
+++ src/main/java/com/gmail/nossr50/config/hocon/superabilities/ConfigSuperAbilities.java
@@ -1,8 +1,33 @@
 package com.gmail.nossr50.config.hocon.superabilities;
 
+import ninja.leaping.configurate.objectmapping.Setting;
 import ninja.leaping.configurate.objectmapping.serialize.ConfigSerializable;
 
 @ConfigSerializable
 public class ConfigSuperAbilities {
 
+    public static final boolean SUPER_ABILITY_DEFAULT = true;
+    public static final boolean MUST_SNEAK_TO_ACTIVATE_DEFAULT = false;
+
+    @Setting(value = "Enable-Super-Abilities",
+            comment = "Turn this off to disable all super abilities." +
+                    "\nDefault value: "+SUPER_ABILITY_DEFAULT)
+    private boolean superAbilitiesEnabled = SUPER_ABILITY_DEFAULT;
+
+    @Setting(value = "Require-Sneaking",
+            comment = "Players must be sneaking in order to activate super abilities." +
+                    "\nDefault value: "+MUST_SNEAK_TO_ACTIVATE_DEFAULT)
+    private boolean mustSneakToActivate = MUST_SNEAK_TO_ACTIVATE_DEFAULT;
+
+    @Setting(value = "Super-Ability-Cooldowns",
+            comment = "How many seconds players must wait before they can use a super ability again.")
+    private ConfigSectionSuperAbilityCooldowns superAbilityCooldowns = new ConfigSectionSuperAbilityCooldowns();
+
+    @Setting(value = "Super-Ability-Max-Length",
+            comment = "The maximum amount of time a player can use a super ability." +
+            "\nMost super abilities get longer as a player grows in skill.")
+    private ConfigSectionSuperAbilityMaxLength superAbilityMaxLength = new ConfigSectionSuperAbilityMaxLength();
+
+    @Setting(value = "Super-Ability-Settings", comment = "Change specific parameters for super abilities.")
+    private ConfigSectionSuperAbilityLimits superAbilityLimits = new ConfigSectionSuperAbilityLimits();
 }
