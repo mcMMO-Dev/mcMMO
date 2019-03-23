df6315dd62a376e7562b6232fa2404ebfec26f90
nossr50 <nossr50@gmail.com>
2019-03-23 16:23:38 -0700

    Fixed Repair Mastery using incorrect numbers during calculations

--- src/main/java/com/gmail/nossr50/skills/repair/Repair.java
+++ src/main/java/com/gmail/nossr50/skills/repair/Repair.java
@@ -2,10 +2,11 @@ package com.gmail.nossr50.skills.repair;
 
 import com.gmail.nossr50.config.AdvancedConfig;
 import com.gmail.nossr50.config.Config;
+import com.gmail.nossr50.datatypes.skills.SubSkillType;
 import org.bukkit.Material;
 
 public class Repair {
-    public static int    repairMasteryMaxBonusLevel = AdvancedConfig.getInstance().getRepairMasteryMaxLevel();
+    public static int    repairMasteryMaxBonusLevel = AdvancedConfig.getInstance().getMaxBonusLevel(SubSkillType.REPAIR_REPAIR_MASTERY);
     public static double repairMasteryMaxBonus      = AdvancedConfig.getInstance().getRepairMasteryMaxBonus();
 
     public static Material anvilMaterial  = Config.getInstance().getRepairAnvilMaterial();
