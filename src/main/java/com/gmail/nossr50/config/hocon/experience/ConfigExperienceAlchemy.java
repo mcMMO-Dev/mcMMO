aecc90fa337b2a185fd9842468158cee3d27f8e6
nossr50 <nossr50@gmail.com>
2019-04-09 20:14:42 -0700

    Correct Alchemy XP values in config defaults

--- src/main/java/com/gmail/nossr50/config/hocon/experience/ConfigExperienceAlchemy.java
+++ src/main/java/com/gmail/nossr50/config/hocon/experience/ConfigExperienceAlchemy.java
@@ -12,15 +12,14 @@ public class ConfigExperienceAlchemy {
 
     static {
        ALCHEMY_DEFAULT_XP_MAP = new HashMap<>();
-       ALCHEMY_DEFAULT_XP_MAP.put("Simple_Potions", 15);
-       ALCHEMY_DEFAULT_XP_MAP.put("Potion_One_Ingredient", 30);
-       ALCHEMY_DEFAULT_XP_MAP.put("Potion_One_Ingredient_One_Amplifier", 60);
-       ALCHEMY_DEFAULT_XP_MAP.put("Potion_One_Ingredient_Two_Amplifier", 120);
-       ALCHEMY_DEFAULT_XP_MAP.put("Potion_One_Ingredient_Swapped_Amplifiers", 240);
+       ALCHEMY_DEFAULT_XP_MAP.put("Stage-One-Potion", 15);
+       ALCHEMY_DEFAULT_XP_MAP.put("Stage-Two-Potion", 30);
+       ALCHEMY_DEFAULT_XP_MAP.put("Stage-Three-Potion", 60);
+       ALCHEMY_DEFAULT_XP_MAP.put("Stage-Four-Potion", 120);
     }
 
     @Setting(value = "Alchemy-Experience-Values", comment = "Experience values for alchemy.")
-    HashMap<String, Integer>alchemyXPMap = ALCHEMY_DEFAULT_XP_MAP;
+    HashMap<String, Integer> alchemyXPMap = ALCHEMY_DEFAULT_XP_MAP;
 
 
     public HashMap<String, Integer> getAlchemyXPMap() {
