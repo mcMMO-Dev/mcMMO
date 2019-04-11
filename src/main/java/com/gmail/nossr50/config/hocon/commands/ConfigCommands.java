b64b8d0f8f344323edcb812a0c082ac544eb211f
nossr50 <nossr50@gmail.com>
2019-04-11 07:09:58 -0700

    Admin Chat config settings

--- src/main/java/com/gmail/nossr50/config/hocon/commands/ConfigCommands.java
+++ src/main/java/com/gmail/nossr50/config/hocon/commands/ConfigCommands.java
@@ -1,7 +1,23 @@
 package com.gmail.nossr50.config.hocon.commands;
 
+import ninja.leaping.configurate.objectmapping.Setting;
 import ninja.leaping.configurate.objectmapping.serialize.ConfigSerializable;
 
 @ConfigSerializable
 public class ConfigCommands {
+
+    @Setting(value = "Admin-Chat", comment = "Settings related to the admin chat command and chatting modes.")
+    private ConfigCommandsAdminChat adminChat = new ConfigCommandsAdminChat();
+
+    public ConfigCommandsAdminChat getAdminChat() {
+        return adminChat;
+    }
+
+    public String getAdminChatPrefix() {
+        return getAdminChat().getAdminChatPrefix();
+    }
+
+    public boolean isUseDisplayNames() {
+        return getAdminChat().isUseDisplayNames();
+    }
 }
