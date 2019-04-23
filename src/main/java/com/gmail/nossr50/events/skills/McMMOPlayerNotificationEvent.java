0adaa0ba6634b16c6d190cc1346cbb416dbc59be
nossr50 <nossr50@gmail.com>
2019-04-23 01:10:09 -0700

    PlayerNotification now marked async

--- src/main/java/com/gmail/nossr50/events/skills/McMMOPlayerNotificationEvent.java
+++ src/main/java/com/gmail/nossr50/events/skills/McMMOPlayerNotificationEvent.java
@@ -5,13 +5,14 @@ import net.md_5.bungee.api.ChatMessageType;
 import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Cancellable;
+import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
 import org.bukkit.event.player.PlayerEvent;
 
 /**
  * This event is sent for when mcMMO informs a player about various important information
  */
-public class McMMOPlayerNotificationEvent extends PlayerEvent implements Cancellable {
+public class McMMOPlayerNotificationEvent extends Event implements Cancellable {
     private boolean isCancelled;
     /*
      * Messages can be sent to both places, as configured in advanced.yml
@@ -27,7 +28,7 @@ public class McMMOPlayerNotificationEvent extends PlayerEvent implements Cancell
     protected final NotificationType notificationType;
 
     public McMMOPlayerNotificationEvent(Player who, NotificationType notificationType, TextComponent notificationTextComponent, ChatMessageType chatMessageType, boolean isMessageAlsoBeingSentToChat) {
-        super(who);
+        super(true);
         this.notificationType = notificationType;
         this.notificationTextComponent = notificationTextComponent;
         this.chatMessageType = chatMessageType;
