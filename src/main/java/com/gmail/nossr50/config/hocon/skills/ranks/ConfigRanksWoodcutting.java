86a46babc14c72ceebd94f66a7f52a2da73eb6dc
nossr50 <nossr50@gmail.com>
2019-06-03 06:09:20 -0700

    Fill in Woodcutting rank config

--- src/main/java/com/gmail/nossr50/config/hocon/skills/ranks/ConfigRanksWoodcutting.java
+++ src/main/java/com/gmail/nossr50/config/hocon/skills/ranks/ConfigRanksWoodcutting.java
@@ -1,8 +1,29 @@
 package com.gmail.nossr50.config.hocon.skills.ranks;
 
+import ninja.leaping.configurate.objectmapping.Setting;
 import ninja.leaping.configurate.objectmapping.serialize.ConfigSerializable;
 
 @ConfigSerializable
 public class ConfigRanksWoodcutting {
 
+    @Setting(value = "Tree-Feller")
+    private SkillRankProperty treeFeller = new SkillRankProperty(5);
+
+    @Setting(value = "Harvest-Lumber")
+    private SkillRankProperty harvestLumber = new SkillRankProperty(5);
+
+    @Setting(value = "Leaf-Blower")
+    private SkillRankProperty leafBlower = new SkillRankProperty(30);
+
+    public SkillRankProperty getTreeFeller() {
+        return treeFeller;
+    }
+
+    public SkillRankProperty getHarvestLumber() {
+        return harvestLumber;
+    }
+
+    public SkillRankProperty getLeafBlower() {
+        return leafBlower;
+    }
 }
