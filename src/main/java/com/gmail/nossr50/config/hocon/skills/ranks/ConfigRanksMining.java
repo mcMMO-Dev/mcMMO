0e70c9ce75995ab1e8c11c987e6054019c32ab19
nossr50 <nossr50@gmail.com>
2019-06-03 05:42:42 -0700

    Fill in Mining rank info

--- src/main/java/com/gmail/nossr50/config/hocon/skills/ranks/ConfigRanksMining.java
+++ src/main/java/com/gmail/nossr50/config/hocon/skills/ranks/ConfigRanksMining.java
@@ -1,8 +1,36 @@
 package com.gmail.nossr50.config.hocon.skills.ranks;
 
+import ninja.leaping.configurate.objectmapping.Setting;
 import ninja.leaping.configurate.objectmapping.serialize.ConfigSerializable;
 
 @ConfigSerializable
 public class ConfigRanksMining {
 
+    @Setting(value = "Super-Breaker")
+    private SkillRankProperty superBreaker = new SkillRankProperty(5);
+
+    @Setting(value = "Bigger-Bombs")
+    private SkillRankProperty biggerBombs = new SkillRankProperty(10);
+
+    @Setting(value = "Demolitions-Expertise")
+    private SkillRankProperty demolitionsExpertise = new SkillRankProperty(10);
+
+    @Setting(value = "Blast-Mining")
+    private SkillRankProperty blastMining = new SkillRankProperty(10, 25, 35, 50, 65, 75, 85, 100);
+
+    public SkillRankProperty getSuperBreaker() {
+        return superBreaker;
+    }
+
+    public SkillRankProperty getBiggerBombs() {
+        return biggerBombs;
+    }
+
+    public SkillRankProperty getDemolitionsExpertise() {
+        return demolitionsExpertise;
+    }
+
+    public SkillRankProperty getBlastMining() {
+        return blastMining;
+    }
 }
