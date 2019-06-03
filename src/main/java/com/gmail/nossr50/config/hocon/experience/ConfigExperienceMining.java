f898525362cd09a62bdc0b1df02ca418ea05e908
nossr50 <nossr50@gmail.com>
2019-06-03 01:57:25 -0700

    Stringify ConfigExperienceMining

--- src/main/java/com/gmail/nossr50/config/hocon/experience/ConfigExperienceMining.java
+++ src/main/java/com/gmail/nossr50/config/hocon/experience/ConfigExperienceMining.java
@@ -15,104 +15,104 @@ public class ConfigExperienceMining {
         MINING_EXPERIENCE_DEFAULT = new HashMap<>();
 
         /* UNDER THE SEA */
-        MINING_EXPERIENCE_DEFAULT.put(Material.TUBE_CORAL_BLOCK.getKey().toString(), 75);
-        MINING_EXPERIENCE_DEFAULT.put(Material.BRAIN_CORAL_BLOCK.getKey().toString(), 85);
-        MINING_EXPERIENCE_DEFAULT.put(Material.BUBBLE_CORAL_BLOCK.getKey().toString(), 70);
-        MINING_EXPERIENCE_DEFAULT.put(Material.FIRE_CORAL_BLOCK.getKey().toString(), 90);
-        MINING_EXPERIENCE_DEFAULT.put(Material.HORN_CORAL_BLOCK.getKey().toString(), 125);
-        MINING_EXPERIENCE_DEFAULT.put(Material.PRISMARINE.getKey().toString(), 30);
-        MINING_EXPERIENCE_DEFAULT.put(Material.PRISMARINE_BRICK_SLAB.getKey().toString(), 30);
-        MINING_EXPERIENCE_DEFAULT.put(Material.PRISMARINE_BRICK_STAIRS.getKey().toString(), 30);
-        MINING_EXPERIENCE_DEFAULT.put(Material.PRISMARINE_BRICKS.getKey().toString(), 30);
-        MINING_EXPERIENCE_DEFAULT.put(Material.PRISMARINE_SLAB.getKey().toString(), 30);
-        MINING_EXPERIENCE_DEFAULT.put(Material.PRISMARINE_STAIRS.getKey().toString(), 30);
-        MINING_EXPERIENCE_DEFAULT.put(Material.DARK_PRISMARINE.getKey().toString(), 30);
-        MINING_EXPERIENCE_DEFAULT.put(Material.DARK_PRISMARINE_SLAB.getKey().toString(), 30);
-        MINING_EXPERIENCE_DEFAULT.put(Material.DARK_PRISMARINE_STAIRS.getKey().toString(), 30);
+        MINING_EXPERIENCE_DEFAULT.put("minecraft:tube_coral_block", 75);
+        MINING_EXPERIENCE_DEFAULT.put("minecraft:brain_coral_block", 85);
+        MINING_EXPERIENCE_DEFAULT.put("minecraft:bubble_coral_block", 70);
+        MINING_EXPERIENCE_DEFAULT.put("minecraft:fire_coral_block", 90);
+        MINING_EXPERIENCE_DEFAULT.put("minecraft:horn_coral_block", 125);
+        MINING_EXPERIENCE_DEFAULT.put("minecraft:prismarine", 30);
+        MINING_EXPERIENCE_DEFAULT.put("minecraft:prismarine_brick_slab", 30);
+        MINING_EXPERIENCE_DEFAULT.put("minecraft:prismarine_brick_stairs", 30);
+        MINING_EXPERIENCE_DEFAULT.put("minecraft:prismarine_bricks", 30);
+        MINING_EXPERIENCE_DEFAULT.put("minecraft:prismarine_slab", 30);
+        MINING_EXPERIENCE_DEFAULT.put("minecraft:prismarine_stairs", 30);
+        MINING_EXPERIENCE_DEFAULT.put("minecraft:dark_prismarine", 30);
+        MINING_EXPERIENCE_DEFAULT.put("minecraft:dark_prismarine_slab", 30);
+        MINING_EXPERIENCE_DEFAULT.put("minecraft:dark_prismarine_stairs", 30);
 
         /* ORE */
-        MINING_EXPERIENCE_DEFAULT.put(Material.COAL_ORE.getKey().toString(), 100);
-        MINING_EXPERIENCE_DEFAULT.put(Material.DIAMOND_ORE.getKey().toString(), 750);
-        MINING_EXPERIENCE_DEFAULT.put(Material.EMERALD_ORE.getKey().toString(), 1000);
-        MINING_EXPERIENCE_DEFAULT.put(Material.GOLD_ORE.getKey().toString(), 350);
-        MINING_EXPERIENCE_DEFAULT.put(Material.IRON_ORE.getKey().toString(), 250);
-        MINING_EXPERIENCE_DEFAULT.put(Material.LAPIS_ORE.getKey().toString(), 400);
-        MINING_EXPERIENCE_DEFAULT.put(Material.NETHER_QUARTZ_ORE.getKey().toString(), 100);
-        MINING_EXPERIENCE_DEFAULT.put(Material.REDSTONE_ORE.getKey().toString(), 150);
+        MINING_EXPERIENCE_DEFAULT.put("minecraft:coal_ore", 100);
+        MINING_EXPERIENCE_DEFAULT.put("minecraft:diamond_ore", 750);
+        MINING_EXPERIENCE_DEFAULT.put("minecraft:emerald_ore", 1000);
+        MINING_EXPERIENCE_DEFAULT.put("minecraft:gold_ore", 350);
+        MINING_EXPERIENCE_DEFAULT.put("minecraft:iron_ore", 250);
+        MINING_EXPERIENCE_DEFAULT.put("minecraft:lapis_ore", 400);
+        MINING_EXPERIENCE_DEFAULT.put("minecraft:nether_quartz_ore", 100);
+        MINING_EXPERIENCE_DEFAULT.put("minecraft:redstone_ore", 150);
 
         /* HELL RELATED */
 
-        MINING_EXPERIENCE_DEFAULT.put(Material.NETHER_BRICK.getKey().toString(), 50);
-        MINING_EXPERIENCE_DEFAULT.put(Material.GLOWSTONE.getKey().toString(), 30);
-        MINING_EXPERIENCE_DEFAULT.put(Material.NETHERRACK.getKey().toString(), 30);
-        MINING_EXPERIENCE_DEFAULT.put(Material.MAGMA_BLOCK.getKey().toString(), 30);
+        MINING_EXPERIENCE_DEFAULT.put("minecraft:nether_brick", 50);
+        MINING_EXPERIENCE_DEFAULT.put("minecraft:glowstone", 30);
+        MINING_EXPERIENCE_DEFAULT.put("minecraft:netherrack", 30);
+        MINING_EXPERIENCE_DEFAULT.put("minecraft:magma_block", 30);
 
         /* BADLANDS BIOME */
-        MINING_EXPERIENCE_DEFAULT.put(Material.TERRACOTTA.getKey().toString(), 30);
-        MINING_EXPERIENCE_DEFAULT.put(Material.BLACK_TERRACOTTA.getKey().toString(), 50);
-        MINING_EXPERIENCE_DEFAULT.put(Material.BLUE_TERRACOTTA.getKey().toString(), 50);
-        MINING_EXPERIENCE_DEFAULT.put(Material.BROWN_TERRACOTTA.getKey().toString(), 50);
-        MINING_EXPERIENCE_DEFAULT.put(Material.CYAN_TERRACOTTA.getKey().toString(), 50);
-        MINING_EXPERIENCE_DEFAULT.put(Material.GRAY_TERRACOTTA.getKey().toString(), 50);
-        MINING_EXPERIENCE_DEFAULT.put(Material.GREEN_TERRACOTTA.getKey().toString(), 50);
-        MINING_EXPERIENCE_DEFAULT.put(Material.LIGHT_BLUE_TERRACOTTA.getKey().toString(), 50);
-        MINING_EXPERIENCE_DEFAULT.put(Material.LIGHT_GRAY_TERRACOTTA.getKey().toString(), 50);
-        MINING_EXPERIENCE_DEFAULT.put(Material.LIME_TERRACOTTA.getKey().toString(), 50);
-        MINING_EXPERIENCE_DEFAULT.put(Material.MAGENTA_TERRACOTTA.getKey().toString(), 50);
-        MINING_EXPERIENCE_DEFAULT.put(Material.ORANGE_TERRACOTTA.getKey().toString(), 50);
-        MINING_EXPERIENCE_DEFAULT.put(Material.PINK_TERRACOTTA.getKey().toString(), 50);
-        MINING_EXPERIENCE_DEFAULT.put(Material.PURPLE_TERRACOTTA.getKey().toString(), 50);
-        MINING_EXPERIENCE_DEFAULT.put(Material.RED_TERRACOTTA.getKey().toString(), 50);
-        MINING_EXPERIENCE_DEFAULT.put(Material.WHITE_TERRACOTTA.getKey().toString(), 50);
-        MINING_EXPERIENCE_DEFAULT.put(Material.YELLOW_TERRACOTTA.getKey().toString(), 50);
+        MINING_EXPERIENCE_DEFAULT.put("minecraft:terracotta", 30);
+        MINING_EXPERIENCE_DEFAULT.put("minecraft:black_terracotta", 50);
+        MINING_EXPERIENCE_DEFAULT.put("minecraft:blue_terracotta", 50);
+        MINING_EXPERIENCE_DEFAULT.put("minecraft:brown_terracotta", 50);
+        MINING_EXPERIENCE_DEFAULT.put("minecraft:cyan_terracotta", 50);
+        MINING_EXPERIENCE_DEFAULT.put("minecraft:gray_terracotta", 50);
+        MINING_EXPERIENCE_DEFAULT.put("minecraft:green_terracotta", 50);
+        MINING_EXPERIENCE_DEFAULT.put("minecraft:light_blue_terracotta", 50);
+        MINING_EXPERIENCE_DEFAULT.put("minecraft:light_gray_terracotta", 50);
+        MINING_EXPERIENCE_DEFAULT.put("minecraft:lime_terracotta", 50);
+        MINING_EXPERIENCE_DEFAULT.put("minecraft:magenta_terracotta", 50);
+        MINING_EXPERIENCE_DEFAULT.put("minecraft:orange_terracotta", 50);
+        MINING_EXPERIENCE_DEFAULT.put("minecraft:pink_terracotta", 50);
+        MINING_EXPERIENCE_DEFAULT.put("minecraft:purple_terracotta", 50);
+        MINING_EXPERIENCE_DEFAULT.put("minecraft:red_terracotta", 50);
+        MINING_EXPERIENCE_DEFAULT.put("minecraft:white_terracotta", 50);
+        MINING_EXPERIENCE_DEFAULT.put("minecraft:yellow_terracotta", 50);
 
         /* COMMON OR BIOME INDEPENDENT MINERALS */
-        MINING_EXPERIENCE_DEFAULT.put(Material.ANDESITE.getKey().toString(), 30);
-        MINING_EXPERIENCE_DEFAULT.put(Material.DIORITE.getKey().toString(), 30);
-        MINING_EXPERIENCE_DEFAULT.put(Material.GRANITE.getKey().toString(), 30);
-        MINING_EXPERIENCE_DEFAULT.put(Material.STONE.getKey().toString(), 30);
-        MINING_EXPERIENCE_DEFAULT.put(Material.MOSSY_COBBLESTONE.getKey().toString(), 30);
-        MINING_EXPERIENCE_DEFAULT.put(Material.MOSSY_COBBLESTONE_SLAB.getKey().toString(), 30);
-        MINING_EXPERIENCE_DEFAULT.put(Material.MOSSY_COBBLESTONE_STAIRS.getKey().toString(), 30);
-        MINING_EXPERIENCE_DEFAULT.put(Material.MOSSY_COBBLESTONE_WALL.getKey().toString(), 30);
-        MINING_EXPERIENCE_DEFAULT.put(Material.MOSSY_STONE_BRICKS.getKey().toString(), 30);
-        MINING_EXPERIENCE_DEFAULT.put(Material.MOSSY_STONE_BRICK_SLAB.getKey().toString(), 30);
-        MINING_EXPERIENCE_DEFAULT.put(Material.MOSSY_STONE_BRICK_WALL.getKey().toString(), 30);
-        MINING_EXPERIENCE_DEFAULT.put(Material.MOSSY_STONE_BRICK_STAIRS.getKey().toString(), 30);
-        MINING_EXPERIENCE_DEFAULT.put(Material.OBSIDIAN.getKey().toString(), 150);
-        MINING_EXPERIENCE_DEFAULT.put(Material.STONE_BRICKS.getKey().toString(), 30);
-        MINING_EXPERIENCE_DEFAULT.put(Material.STONE_BRICK_SLAB.getKey().toString(), 30);
-        MINING_EXPERIENCE_DEFAULT.put(Material.STONE_BRICK_STAIRS.getKey().toString(), 30);
-        MINING_EXPERIENCE_DEFAULT.put(Material.STONE_BRICK_STAIRS.getKey().toString(), 30);
-        MINING_EXPERIENCE_DEFAULT.put(Material.CRACKED_STONE_BRICKS.getKey().toString(), 30);
-        MINING_EXPERIENCE_DEFAULT.put(Material.INFESTED_CRACKED_STONE_BRICKS.getKey().toString(), 30);
-        MINING_EXPERIENCE_DEFAULT.put(Material.CHISELED_QUARTZ_BLOCK.getKey().toString(), 30);
-        MINING_EXPERIENCE_DEFAULT.put(Material.CHISELED_RED_SANDSTONE.getKey().toString(), 30);
-        MINING_EXPERIENCE_DEFAULT.put(Material.CHISELED_SANDSTONE.getKey().toString(), 30);
-        MINING_EXPERIENCE_DEFAULT.put(Material.CHISELED_STONE_BRICKS.getKey().toString(), 30);
-        MINING_EXPERIENCE_DEFAULT.put(Material.INFESTED_CHISELED_STONE_BRICKS.getKey().toString(), 30);
+        MINING_EXPERIENCE_DEFAULT.put("minecraft:andesite", 30);
+        MINING_EXPERIENCE_DEFAULT.put("minecraft:diorite", 30);
+        MINING_EXPERIENCE_DEFAULT.put("minecraft:granite", 30);
+        MINING_EXPERIENCE_DEFAULT.put("minecraft:stone", 30);
+        MINING_EXPERIENCE_DEFAULT.put("minecraft:mossy_cobblestone", 30);
+        MINING_EXPERIENCE_DEFAULT.put("minecraft:mossy_cobblestone_slab", 30);
+        MINING_EXPERIENCE_DEFAULT.put("minecraft:mossy_cobblestone_stairs", 30);
+        MINING_EXPERIENCE_DEFAULT.put("minecraft:mossy_cobblestone_wall", 30);
+        MINING_EXPERIENCE_DEFAULT.put("minecraft:mossy_stone_bricks", 30);
+        MINING_EXPERIENCE_DEFAULT.put("minecraft:mossy_stone_brick_slab", 30);
+        MINING_EXPERIENCE_DEFAULT.put("minecraft:mossy_stone_brick_wall", 30);
+        MINING_EXPERIENCE_DEFAULT.put("minecraft:mossy_stone_brick_stairs", 30);
+        MINING_EXPERIENCE_DEFAULT.put("minecraft:obsidian", 150);
+        MINING_EXPERIENCE_DEFAULT.put("minecraft:stone_bricks", 30);
+        MINING_EXPERIENCE_DEFAULT.put("minecraft:stone_brick_slab", 30);
+        MINING_EXPERIENCE_DEFAULT.put("minecraft:stone_brick_stairs", 30);
+        MINING_EXPERIENCE_DEFAULT.put("minecraft:stone_brick_stairs", 30);
+        MINING_EXPERIENCE_DEFAULT.put("minecraft:cracked_stone_bricks", 30);
+        MINING_EXPERIENCE_DEFAULT.put("minecraft:infested_cracked_stone_bricks", 30);
+        MINING_EXPERIENCE_DEFAULT.put("minecraft:chiseled_quartz_block", 30);
+        MINING_EXPERIENCE_DEFAULT.put("minecraft:chiseled_red_sandstone", 30);
+        MINING_EXPERIENCE_DEFAULT.put("minecraft:chiseled_sandstone", 30);
+        MINING_EXPERIENCE_DEFAULT.put("minecraft:chiseled_stone_bricks", 30);
+        MINING_EXPERIENCE_DEFAULT.put("minecraft:infested_chiseled_stone_bricks", 30);
 
         /* SNOWY BIOME */
-        MINING_EXPERIENCE_DEFAULT.put(Material.BLUE_ICE.getKey().toString(), 50);
-        MINING_EXPERIENCE_DEFAULT.put(Material.PACKED_ICE.getKey().toString(), 100);
+        MINING_EXPERIENCE_DEFAULT.put("minecraft:blue_ice", 50);
+        MINING_EXPERIENCE_DEFAULT.put("minecraft:packed_ice", 100);
 
         /* DESERT BIOME */
-        MINING_EXPERIENCE_DEFAULT.put(Material.SANDSTONE.getKey().toString(), 30);
-        MINING_EXPERIENCE_DEFAULT.put(Material.SANDSTONE_SLAB.getKey().toString(), 30);
-        MINING_EXPERIENCE_DEFAULT.put(Material.SANDSTONE_STAIRS.getKey().toString(), 30);
-        MINING_EXPERIENCE_DEFAULT.put(Material.SMOOTH_SANDSTONE.getKey().toString(), 30);
-        MINING_EXPERIENCE_DEFAULT.put(Material.RED_SANDSTONE.getKey().toString(), 30);
-        MINING_EXPERIENCE_DEFAULT.put(Material.RED_SANDSTONE_SLAB.getKey().toString(), 30);
-        MINING_EXPERIENCE_DEFAULT.put(Material.RED_SANDSTONE_STAIRS.getKey().toString(), 30);
-        MINING_EXPERIENCE_DEFAULT.put(Material.SMOOTH_RED_SANDSTONE.getKey().toString(), 30);
+        MINING_EXPERIENCE_DEFAULT.put("minecraft:sandstone", 30);
+        MINING_EXPERIENCE_DEFAULT.put("minecraft:sandstone_slab", 30);
+        MINING_EXPERIENCE_DEFAULT.put("minecraft:sandstone_stairs", 30);
+        MINING_EXPERIENCE_DEFAULT.put("minecraft:smooth_sandstone", 30);
+        MINING_EXPERIENCE_DEFAULT.put("minecraft:red_sandstone", 30);
+        MINING_EXPERIENCE_DEFAULT.put("minecraft:red_sandstone_slab", 30);
+        MINING_EXPERIENCE_DEFAULT.put("minecraft:red_sandstone_stairs", 30);
+        MINING_EXPERIENCE_DEFAULT.put("minecraft:smooth_red_sandstone", 30);
 
         /* END RELATED */
-        MINING_EXPERIENCE_DEFAULT.put(Material.END_STONE.getKey().toString(), 30);
-        MINING_EXPERIENCE_DEFAULT.put(Material.END_STONE_BRICKS.getKey().toString(), 200);
-        MINING_EXPERIENCE_DEFAULT.put(Material.PURPUR_BLOCK.getKey().toString(), 200);
-        MINING_EXPERIENCE_DEFAULT.put(Material.PURPUR_PILLAR.getKey().toString(), 250);
-        MINING_EXPERIENCE_DEFAULT.put(Material.PURPUR_SLAB.getKey().toString(), 150);
-        MINING_EXPERIENCE_DEFAULT.put(Material.PURPUR_STAIRS.getKey().toString(), 250);
+        MINING_EXPERIENCE_DEFAULT.put("minecraft:end_stone", 30);
+        MINING_EXPERIENCE_DEFAULT.put("minecraft:end_stone_bricks", 200);
+        MINING_EXPERIENCE_DEFAULT.put("minecraft:purpur_block", 200);
+        MINING_EXPERIENCE_DEFAULT.put("minecraft:purpur_pillar", 250);
+        MINING_EXPERIENCE_DEFAULT.put("minecraft:purpur_slab", 150);
+        MINING_EXPERIENCE_DEFAULT.put("minecraft:purpur_stairs", 250);
     }
 
     @Setting(value = "Mining-Experience")
