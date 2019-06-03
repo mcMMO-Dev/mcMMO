bdc44f6ede88c857a88086563d3a8a3e06a59ac2
nossr50 <nossr50@gmail.com>
2019-06-03 05:44:22 -0700

    Fill in rank config for Herbalism

--- src/main/java/com/gmail/nossr50/config/hocon/skills/ranks/ConfigRanksHerbalism.java
+++ src/main/java/com/gmail/nossr50/config/hocon/skills/ranks/ConfigRanksHerbalism.java
@@ -1,7 +1,29 @@
 package com.gmail.nossr50.config.hocon.skills.ranks;
 
+import ninja.leaping.configurate.objectmapping.Setting;
 import ninja.leaping.configurate.objectmapping.serialize.ConfigSerializable;
 
 @ConfigSerializable
 public class ConfigRanksHerbalism {
+
+    @Setting(value = "Green-Terra")
+    private SkillRankProperty greenTerra = new SkillRankProperty(5);
+
+    @Setting(value = "Green-Thumb")
+    private SkillRankProperty greenThumb = new SkillRankProperty(25, 50, 75, 100);
+
+    @Setting(value = "Farmers-Diet")
+    private SkillRankProperty farmersDiet = new SkillRankProperty(20, 40, 60, 80, 100);
+
+    public SkillRankProperty getGreenTerra() {
+        return greenTerra;
+    }
+
+    public SkillRankProperty getGreenThumb() {
+        return greenThumb;
+    }
+
+    public SkillRankProperty getFarmersDiet() {
+        return farmersDiet;
+    }
 }
