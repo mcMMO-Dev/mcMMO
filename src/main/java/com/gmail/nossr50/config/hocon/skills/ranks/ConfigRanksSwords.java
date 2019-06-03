9db152ebc88cbd9cd2089d7d6d9d987e7d2307c9
nossr50 <nossr50@gmail.com>
2019-06-03 05:52:53 -0700

    Fill in Swords rank config

--- src/main/java/com/gmail/nossr50/config/hocon/skills/ranks/ConfigRanksSwords.java
+++ src/main/java/com/gmail/nossr50/config/hocon/skills/ranks/ConfigRanksSwords.java
@@ -1,8 +1,43 @@
 package com.gmail.nossr50.config.hocon.skills.ranks;
 
+import ninja.leaping.configurate.objectmapping.Setting;
 import ninja.leaping.configurate.objectmapping.serialize.ConfigSerializable;
 
 @ConfigSerializable
 public class ConfigRanksSwords {
 
+    @Setting(value = "Limit-Break")
+    private SkillRankProperty limitBreak = new SkillRankProperty(10, 20, 30, 40, 50, 60, 70, 80, 90, 100);
+
+    @Setting(value = "Stab")
+    private SkillRankProperty stab = new SkillRankProperty(75, 100);
+
+    @Setting(value = "Counter-Attack")
+    private SkillRankProperty counterAttack = new SkillRankProperty(20);
+
+    @Setting(value = "Rupture")
+    private SkillRankProperty rupture = new SkillRankProperty(5, 15, 75, 90);
+
+    @Setting(value = "Serrated-Strikes")
+    private SkillRankProperty serratedStrikes = new SkillRankProperty(5);
+
+    public SkillRankProperty getLimitBreak() {
+        return limitBreak;
+    }
+
+    public SkillRankProperty getStab() {
+        return stab;
+    }
+
+    public SkillRankProperty getCounterAttack() {
+        return counterAttack;
+    }
+
+    public SkillRankProperty getRupture() {
+        return rupture;
+    }
+
+    public SkillRankProperty getSerratedStrikes() {
+        return serratedStrikes;
+    }
 }
