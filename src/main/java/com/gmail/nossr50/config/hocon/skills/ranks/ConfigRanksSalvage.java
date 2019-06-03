2d1d2693ed76d512f5ca06aa047fa2e6c53a62a6
nossr50 <nossr50@gmail.com>
2019-06-03 04:44:16 -0700

    Fill in Salvage rank information

--- src/main/java/com/gmail/nossr50/config/hocon/skills/ranks/ConfigRanksSalvage.java
+++ src/main/java/com/gmail/nossr50/config/hocon/skills/ranks/ConfigRanksSalvage.java
@@ -1,8 +1,22 @@
 package com.gmail.nossr50.config.hocon.skills.ranks;
 
+import ninja.leaping.configurate.objectmapping.Setting;
 import ninja.leaping.configurate.objectmapping.serialize.ConfigSerializable;
 
 @ConfigSerializable
 public class ConfigRanksSalvage {
 
+    @Setting(value = "Advanced-Salvage")
+    private SkillRankProperty advancedSalvage = new SkillRankProperty(35);
+
+    @Setting(value = "Arcane-Salvage")
+    private SkillRankProperty arcaneSalvage = new SkillRankProperty(10, 25, 35, 50, 65, 75, 85, 100);
+
+    public SkillRankProperty getAdvancedSalvage() {
+        return advancedSalvage;
+    }
+
+    public SkillRankProperty getArcaneSalvage() {
+        return arcaneSalvage;
+    }
 }
