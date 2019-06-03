29876b4a20c2e11368a62e589b3b89880c72458f
nossr50 <nossr50@gmail.com>
2019-06-02 23:24:30 -0700

    SkillProperty no longer has an identifier for SubSkillType

--- src/main/java/com/gmail/nossr50/datatypes/skills/properties/AbstractMaximumProgressionLevel.java
+++ src/main/java/com/gmail/nossr50/datatypes/skills/properties/AbstractMaximumProgressionLevel.java
@@ -1,23 +1,13 @@
 package com.gmail.nossr50.datatypes.skills.properties;
 
-import com.gmail.nossr50.datatypes.skills.SubSkillType;
-
 public class AbstractMaximumProgressionLevel implements MaximumProgressionLevel {
-
-    private SubSkillType subSkillType;
 
     private int standardMaxLevel;
     private int retroMaxLevel;
 
-    public AbstractMaximumProgressionLevel(SubSkillType subSkillType, int standardMaxLevel, int retroMaxLevel) {
-        this.subSkillType = subSkillType;
+    public AbstractMaximumProgressionLevel(int standardMaxLevel, int retroMaxLevel) {
         this.standardMaxLevel = standardMaxLevel;
         this.retroMaxLevel = retroMaxLevel;
-    }
-
-    @Override
-    public SubSkillType getSubSkillType() {
-        return subSkillType;
     }
 
     @Override
--- src/main/java/com/gmail/nossr50/datatypes/skills/properties/SkillProperty.java
+++ src/main/java/com/gmail/nossr50/datatypes/skills/properties/SkillProperty.java
@@ -1,10 +1,8 @@
 package com.gmail.nossr50.datatypes.skills.properties;
 
-import com.gmail.nossr50.datatypes.skills.SubSkillType;
-
 /**
  * Skill Properties are various values used for interactions of sub-skills
  */
 public interface SkillProperty {
-    SubSkillType getSubSkillType();
+
 }
