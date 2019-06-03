b62bd0e620c4bac7b23017a6ce1cedf640545119
nossr50 <nossr50@gmail.com>
2019-06-03 06:13:35 -0700

    Fill in Excavation rank config

--- src/main/java/com/gmail/nossr50/config/hocon/skills/ranks/ConfigRanksExcavation.java
+++ src/main/java/com/gmail/nossr50/config/hocon/skills/ranks/ConfigRanksExcavation.java
@@ -1,8 +1,22 @@
 package com.gmail.nossr50.config.hocon.skills.ranks;
 
+import ninja.leaping.configurate.objectmapping.Setting;
 import ninja.leaping.configurate.objectmapping.serialize.ConfigSerializable;
 
 @ConfigSerializable
 public class ConfigRanksExcavation {
 
+    @Setting(value = "Giga-Drill-Breaker")
+    private SkillRankProperty gigaDrillBreaker = new SkillRankProperty(5);
+
+    @Setting(value = "Archaeology")
+    private SkillRankProperty archaeology = new SkillRankProperty(5, 25, 35, 50, 65, 75, 85, 100);
+
+    public SkillRankProperty getGigaDrillBreaker() {
+        return gigaDrillBreaker;
+    }
+
+    public SkillRankProperty getArchaeology() {
+        return archaeology;
+    }
 }
