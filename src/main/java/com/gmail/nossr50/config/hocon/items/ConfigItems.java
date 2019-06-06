240ab0a0af965bde56841e8f9af72b6e4d602da3
nossr50 <nossr50@gmail.com>
2019-06-06 03:38:09 -0700

    Add Chimaera Wing to the items config

--- src/main/java/com/gmail/nossr50/config/hocon/items/ConfigItems.java
+++ src/main/java/com/gmail/nossr50/config/hocon/items/ConfigItems.java
@@ -1,8 +1,59 @@
 package com.gmail.nossr50.config.hocon.items;
 
+import ninja.leaping.configurate.objectmapping.Setting;
 import ninja.leaping.configurate.objectmapping.serialize.ConfigSerializable;
 
 @ConfigSerializable
 public class ConfigItems {
 
+    @Setting(value = "Consumables", comment = "Settings for items that get consumed after one use.")
+    private ConfigItemsConsumables consumables = new ConfigItemsConsumables();
+
+    public ConfigItemsConsumables getConsumables() {
+        return consumables;
+    }
+
+    public ConfigItemsChimaeraWing getChimaeraWing() {
+        return consumables.getChimaeraWing();
+    }
+
+    public int getUseCost() {
+        return getChimaeraWing().getUseCost();
+    }
+
+    public int getRecipeCost() {
+        return getChimaeraWing().getRecipeCost();
+    }
+
+    public String getRecipeMats() {
+        return getChimaeraWing().getRecipeMats();
+    }
+
+    public int getWarmup() {
+        return getChimaeraWing().getWarmup();
+    }
+
+    public int getCooldown() {
+        return getChimaeraWing().getCooldown();
+    }
+
+    public boolean isEnabled() {
+        return getChimaeraWing().isEnabled();
+    }
+
+    public int getRecentlyHurtCooldown() {
+        return getChimaeraWing().getRecentlyHurtCooldown();
+    }
+
+    public boolean isPreventUndergroundUse() {
+        return getChimaeraWing().isPreventUndergroundUse();
+    }
+
+    public boolean isUseBedSpawn() {
+        return getChimaeraWing().isUseBedSpawn();
+    }
+
+    public boolean isSoundEnabled() {
+        return getChimaeraWing().isSoundEnabled();
+    }
 }
---
+++ src/main/java/com/gmail/nossr50/config/hocon/items/ConfigItemsConsumables.java
@@ -0,0 +1,17 @@
+package com.gmail.nossr50.config.hocon.items;
+
+import ninja.leaping.configurate.objectmapping.Setting;
+import ninja.leaping.configurate.objectmapping.serialize.ConfigSerializable;
+
+@ConfigSerializable
+public class ConfigItemsConsumables {
+
+    @Setting(value = "Chimaera-Wing", comment = "Settings relating to the Chimaera Wing." +
+            "\nThe CW is an item in mcMMO that will teleport players to the bed they last rested at as long as they do not have any solid blocks above their head." +
+            "\nThe CW is crafted using a custom recipe.")
+    private ConfigItemsChimaeraWing chimaeraWing = new ConfigItemsChimaeraWing();
+
+    public ConfigItemsChimaeraWing getChimaeraWing() {
+        return chimaeraWing;
+    }
+}
