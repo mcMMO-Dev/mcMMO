51a57429b6b8e3480a726592b7d032f4129d1e53
nossr50 <nossr50@gmail.com>
2019-06-06 05:58:44 -0700

    Add PVP/PVE settings to core skill config

--- src/main/java/com/gmail/nossr50/config/hocon/skills/coreskills/ConfigCoreSkills.java
+++ src/main/java/com/gmail/nossr50/config/hocon/skills/coreskills/ConfigCoreSkills.java
@@ -1,5 +1,6 @@
 package com.gmail.nossr50.config.hocon.skills.coreskills;
 
+import com.gmail.nossr50.datatypes.skills.PrimarySkillType;
 import ninja.leaping.configurate.objectmapping.Setting;
 import ninja.leaping.configurate.objectmapping.serialize.ConfigSerializable;
 
@@ -9,6 +10,10 @@ public class ConfigCoreSkills {
     @Setting(value = "Core Skills for Acrobatics" +
             "\nCore skills are ones built into mcMMO")
     private ConfigCoreSkillsAcrobatics configCoreSkillsAcrobatics = new ConfigCoreSkillsAcrobatics();
+
+    @Setting(value = "Combat-Settings", comment = "Determine whether or not a skills effects can activate in PVP or PVE" +
+            "\nIf a skill has no combat interactions, the toggle for it will still exists in case I ever do add combat interactions for the skill.")
+    private ConfigCoreSkillsCombatToggles combatToggles = new ConfigCoreSkillsCombatToggles();
 
     public boolean isRollEnabled() {
         return configCoreSkillsAcrobatics.isRollEnabled();
@@ -20,5 +25,25 @@ public class ConfigCoreSkills {
 
     public boolean isAcrobaticsEnabled() {
         return getConfigCoreSkillsAcrobatics().isAcrobaticsEnabled();
+    }
+
+    public ConfigCoreSkillsCombatToggles getCombatToggles() {
+        return combatToggles;
+    }
+
+    public ConfigCoreSkillCombatMap getPve() {
+        return combatToggles.getPve();
+    }
+
+    public ConfigCoreSkillCombatMap getPvp() {
+        return combatToggles.getPvp();
+    }
+
+    public boolean isPVEEnabled(PrimarySkillType primarySkillType) {
+        return combatToggles.isPVEEnabled(primarySkillType);
+    }
+
+    public boolean isPVPEnabled(PrimarySkillType primarySkillType) {
+        return combatToggles.isPVPEnabled(primarySkillType);
     }
 }
