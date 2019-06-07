7679afd3e0792cdd2b3157cdb42a0ac0c327d7fd
nossr50 <nossr50@gmail.com>
2019-06-06 23:15:18 -0700

    Add new DatabaseAPI

---
+++ src/main/java/com/gmail/nossr50/api/DatabaseAPI.java
@@ -0,0 +1,33 @@
+package com.gmail.nossr50.api;
+
+import com.gmail.nossr50.datatypes.player.PlayerProfile;
+import com.gmail.nossr50.mcMMO;
+
+import java.util.UUID;
+
+public class DatabaseAPI {
+
+    /**
+     * Checks if a player exists in the mcMMO Database
+     * @param uuid player UUID
+     * @return true if the player exists in the DB, false if they do not
+     */
+    public boolean doesPlayerExistInDB(String uuid) {
+        return doesPlayerExistInDB(UUID.fromString(uuid));
+    }
+
+    /**
+     * Checks if a player exists in the mcMMO Database
+     * @param uuid player UUID
+     * @return true if the player exists in the DB, false if they do not
+     */
+    public boolean doesPlayerExistInDB(UUID uuid) {
+        PlayerProfile playerProfile = mcMMO.getDatabaseManager().loadPlayerProfile(uuid);
+
+        if(playerProfile.isLoaded())
+            return true;
+        else
+            return false;
+    }
+
+}
