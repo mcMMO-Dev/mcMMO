4f2e3f4399092524f609a8c2e5ed39c87e41a361
nossr50 <nossr50@gmail.com>
2019-06-14 15:51:26 -0700

    Update axes to the new naming scheme

--- src/main/java/com/gmail/nossr50/config/hocon/skills/axes/ConfigAxesCriticalStrikes.java
+++ src/main/java/com/gmail/nossr50/config/hocon/skills/axes/ConfigAxesCriticalStrikes.java
@@ -1,5 +1,6 @@
 package com.gmail.nossr50.config.hocon.skills.axes;
 
+import com.gmail.nossr50.config.ConfigConstants;
 import com.gmail.nossr50.datatypes.skills.properties.AbstractDamageProperty;
 import com.gmail.nossr50.datatypes.skills.properties.AbstractSkillCeiling;
 import com.gmail.nossr50.datatypes.skills.properties.DamageProperty;
@@ -11,11 +12,11 @@ public class ConfigAxesCriticalStrikes {
 
     private static final double MAX_ACTIVATION_CHANCE_DEFAULT = 37.50D;
 
-    @Setting(value = "Max-Activation-Chance", comment = "This is max percentage chance that is used to determine whether or not the ability is successful." +
+    @Setting(value = ConfigConstants.MAX_CHANCE_FIELD_NAME, comment = "This is max percentage chance that is used to determine whether or not the ability is successful." +
             "\nA value of 50.0 would mean at most the ability can only have a 50% chance to work at max skill level.")
     private double maxActivationChance = MAX_ACTIVATION_CHANCE_DEFAULT;
 
-    @Setting(value = "Maximum-Level", comment = "This is the level at which full benefits for this skill will be reached." +
+    @Setting(value = ConfigConstants.MAX_BONUS_LEVEL_FIELD_NAME, comment = "This is the level at which full benefits for this skill will be reached." +
             "\nProperties of this skill may or may not scale with level, but those that do will gradually increase until max level is achieved.")
     private AbstractSkillCeiling maximumProgressionLevel = new AbstractSkillCeiling(100, 1000);
 
