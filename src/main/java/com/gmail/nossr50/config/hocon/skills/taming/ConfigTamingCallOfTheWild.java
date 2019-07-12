fdc8e4b81daf36b33a76cde278c6394cc5c16da3
nossr50 <nossr50@gmail.com>
2019-07-12 10:54:03 -0700

    Actually I should merge from master before working on this further

--- src/main/java/com/gmail/nossr50/config/hocon/skills/taming/ConfigTamingCallOfTheWild.java
+++ src/main/java/com/gmail/nossr50/config/hocon/skills/taming/ConfigTamingCallOfTheWild.java
@@ -1,8 +1,26 @@
 package com.gmail.nossr50.config.hocon.skills.taming;
 
+import ninja.leaping.configurate.objectmapping.Setting;
 import ninja.leaping.configurate.objectmapping.serialize.ConfigSerializable;
+
 
 @ConfigSerializable
 public class ConfigTamingCallOfTheWild {
 
+    @Setting(value = "Cat-Summon-Item-Name", comment = "The name of the item used to summon cats.")
+    private String cotwCatItem = "minecraft:raw_cod";
+    private String cotwWolfItem = "minecraft:bone";
+    private String cotwHorseItem = "minecraft:apple";
+
+    public String getCotwCatItem() {
+        return cotwCatItem;
+    }
+
+    public String getCotwWolfItem() {
+        return cotwWolfItem;
+    }
+
+    public String getCotwHorseItem() {
+        return cotwHorseItem;
+    }
 }
