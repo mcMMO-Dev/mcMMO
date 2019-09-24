68f6fea66a13cb68101a7946836970579cde1a10
nossr50 <nossr50@gmail.com>
2019-09-24 14:35:55 -0700

    Fix the serialized config loader

--- src/main/java/com/gmail/nossr50/config/SkillConfigFactory.java
+++ src/main/java/com/gmail/nossr50/config/SkillConfigFactory.java
@@ -2,12 +2,13 @@ package com.gmail.nossr50.config;
 
 import com.gmail.nossr50.config.hocon.SerializedConfigLoader;
 import com.gmail.nossr50.datatypes.skills.PrimarySkillType;
+import com.gmail.nossr50.mcMMO;
 import com.gmail.nossr50.util.StringUtils;
 
 public class SkillConfigFactory {
 
-    protected static SerializedConfigLoader initSkillConfig(PrimarySkillType primarySkillType, Class<?> clazz) {
-        return new SerializedConfigLoader(clazz,
+    protected static SerializedConfigLoader initSkillConfig(mcMMO pluginRef, PrimarySkillType primarySkillType, Class<?> clazz) {
+        return new SerializedConfigLoader(pluginRef, clazz,
                 primarySkillType.toString().toLowerCase() + ".conf",
                 StringUtils.getCapitalized(primarySkillType.toString()),
                 null);
