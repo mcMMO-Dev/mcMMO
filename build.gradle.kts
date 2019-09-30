83373275f720ed29f5020b482644fcaf7440354a
Shane Freeder <theboyetronic@gmail.com>
2019-09-30 20:53:44 +0100

    Move mcmmo into submodule
    Goal will be to slowly move out the bukkit* dependencies from -core,
    but, we have our starting point

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,65 +1,26 @@
-import org.apache.tools.ant.filters.ReplaceTokens
+subprojects {
+    apply(plugin = "java-library")
+    // Java is not explicitly needed, but keeps IJ happy with the tasks block
+    apply(plugin = "java")
 
-plugins {
-    `java-library`
-    id("com.github.johnrengelman.shadow") version "5.1.0"
-}
-repositories {
-    mavenLocal()
-    maven("https://repo.spongepowered.org/maven")
-    maven("https://hub.spigotmc.org/nexus/content/repositories/snapshoits")
-    maven("https://repo.codemc.org/repository/maven-public")
-    maven("https://maven.sk89q.com/repo")
-    maven("https://mvnrepository.com/artifact/org.jetbrains/annotations")
-}
-
-tasks {
-    withType(JavaCompile::class.java) {
-        options.encoding = "UTF-8"
+    repositories {
+        mavenLocal()
+        maven("https://repo.spongepowered.org/maven")
+        maven("https://hub.spigotmc.org/nexus/content/repositories/snapshoits")
+        maven("https://repo.codemc.org/repository/maven-public")
+        maven("https://maven.sk89q.com/repo")
+        maven("https://mvnrepository.com/artifact/org.jetbrains/annotations")
     }
 
-    shadowJar {
-        dependencies {
-            include(dependency("org.spongepowered:configurate-yaml"))
-            include(dependency("org.spongepowered:configurate-hocon"))
-            include(dependency("org.spongepowered:configurate-core"))
-            include(dependency("org.bstats:bstats-bukkit"))
-            include(dependency("org.apache.tomcat:tomcat-jdbc"))
-            include(dependency("org.apache.tomcat:tomcat-juli"))
-            include(dependency("com.typesafe:config"))
-            exclude(dependency("org.spigotmc:spigot"))
-        }
-        relocate("org.apache.commons.logging", "com.gmail.nossr50.commons.logging")
-        relocate("org.apache.juli", "com.gmail.nossr50.database.tomcat.juli")
-        relocate("org.apache.tomcat", "com.gmail.nossr50.database.tomcat")
-        relocate("org.bstats", "com.gmail.nossr50.metrics.bstat")
-    }
-
-    processResources {
-        filter<ReplaceTokens>("tokens" to mapOf("project.version" to project.version))
-        filesMatching("**/locales/*") {
-
+    tasks {
+        withType(JavaCompile::class.java) {
+            options.encoding = "UTF-8"
         }
     }
+
+    configure<JavaPluginConvention> {
+        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_1_8
+    }
+
 }
-
-java {
-    targetCompatibility = JavaVersion.VERSION_1_8
-    sourceCompatibility = JavaVersion.VERSION_1_8
-}
-
-dependencies {
-    api("org.apache.tomcat:tomcat-jdbc:7.0.52")
-    api("com.typesafe:config:1.3.2")
-    api("org.spongepowered:configurate-core:3.7-SNAPSHOT")
-    api("org.spongepowered:configurate-yaml:3.7-SNAPSHOT")
-    api("org.spongepowered:configurate-hocon:3.7-SNAPSHOT")
-    implementation("org.jetbrains:annotations:17.0.0")
-    implementation("org.apache.maven.scm:maven-scm-provider-gitexe:1.8.1")
-    implementation("org.bstats:bstats-bukkit:1.4")
-    implementation("org.spigotmc:spigot:1.14.4-R0.1-SNAPSHOT")
-    implementation("com.sk89q.worldguard:worldguard-legacy:7.0.0-SNAPSHOT")
-    testImplementation("junit:junit:4.10")
-}
-
-
---
+++ mcmmo-core/build.gradle.kts
@@ -0,0 +1,50 @@
+import org.apache.tools.ant.filters.ReplaceTokens
+
+plugins {
+    `java-library`
+    id("com.github.johnrengelman.shadow") version "5.1.0"
+}
+
+tasks {
+
+    shadowJar {
+        dependencies {
+            include(dependency("org.spongepowered:configurate-yaml"))
+            include(dependency("org.spongepowered:configurate-hocon"))
+            include(dependency("org.spongepowered:configurate-core"))
+            include(dependency("org.bstats:bstats-bukkit"))
+            include(dependency("org.apache.tomcat:tomcat-jdbc"))
+            include(dependency("org.apache.tomcat:tomcat-juli"))
+            include(dependency("com.typesafe:config"))
+            exclude(dependency("org.spigotmc:spigot"))
+        }
+        relocate("org.apache.commons.logging", "com.gmail.nossr50.commons.logging")
+        relocate("org.apache.juli", "com.gmail.nossr50.database.tomcat.juli")
+        relocate("org.apache.tomcat", "com.gmail.nossr50.database.tomcat")
+        relocate("org.bstats", "com.gmail.nossr50.metrics.bstat")
+    }
+
+    processResources {
+        filter<ReplaceTokens>("tokens" to mapOf("project.version" to project.version))
+        filesMatching("**/locales/*") {
+
+        }
+    }
+}
+
+
+dependencies {
+    api("org.apache.tomcat:tomcat-jdbc:7.0.52")
+    api("com.typesafe:config:1.3.2")
+    api("org.spongepowered:configurate-core:3.7-SNAPSHOT")
+    api("org.spongepowered:configurate-yaml:3.7-SNAPSHOT")
+    api("org.spongepowered:configurate-hocon:3.7-SNAPSHOT")
+    implementation("org.jetbrains:annotations:17.0.0")
+    implementation("org.apache.maven.scm:maven-scm-provider-gitexe:1.8.1")
+    implementation("org.bstats:bstats-bukkit:1.4")
+    implementation("org.spigotmc:spigot:1.14.4-R0.1-SNAPSHOT")
+    implementation("com.sk89q.worldguard:worldguard-legacy:7.0.0-SNAPSHOT")
+    testImplementation("junit:junit:4.10")
+}
+
+
