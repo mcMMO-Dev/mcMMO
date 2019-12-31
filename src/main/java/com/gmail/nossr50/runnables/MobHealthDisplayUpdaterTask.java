a35955bab75ee9d6f7c89bc21a99a33abd9a6210
OmerBenGera <omerthepro126@gmail.com>
2019-12-31 14:11:11 +0200

    Fixed an issue with caching healthbar values.

--- src/main/java/com/gmail/nossr50/runnables/MobHealthDisplayUpdaterTask.java
+++ src/main/java/com/gmail/nossr50/runnables/MobHealthDisplayUpdaterTask.java
@@ -6,22 +6,26 @@ import org.bukkit.scheduler.BukkitRunnable;
 
 public class MobHealthDisplayUpdaterTask extends BukkitRunnable {
     private LivingEntity target;
-    private String oldName;
-    private boolean oldNameVisible;
+    //private String oldName;
+    //private boolean oldNameVisible;
 
     public MobHealthDisplayUpdaterTask(LivingEntity target) {
         if (target.isValid()) {
             this.target = target;
-            this.oldName = target.getMetadata(mcMMO.customNameKey).get(0).asString();
-            this.oldNameVisible = target.getMetadata(mcMMO.customVisibleKey).get(0).asBoolean();
+            // Instead of caching, we will get values later from the Metadata.
+            //this.oldName = target.getMetadata(mcMMO.customNameKey).get(0).asString();
+            //this.oldNameVisible = target.getMetadata(mcMMO.customVisibleKey).get(0).asBoolean();
         }
     }
 
     @Override
     public void run() {
         if (target != null && target.isValid()) {
-            target.setCustomNameVisible(oldNameVisible);
-            target.setCustomName(oldName);
+            // Not caching values anymore.
+            //target.setCustomNameVisible(oldNameVisible);
+            //target.setCustomName(oldName);
+            target.setCustomNameVisible(target.getMetadata(mcMMO.customVisibleKey).get(0).asBoolean());
+            target.setCustomName(target.getMetadata(mcMMO.customNameKey).get(0).asString());
             target.removeMetadata(mcMMO.customNameKey, mcMMO.p);
             target.removeMetadata(mcMMO.customVisibleKey, mcMMO.p);
         }
