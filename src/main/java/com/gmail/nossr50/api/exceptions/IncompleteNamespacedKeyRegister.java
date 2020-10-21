65eea55e620772176abc1e4604cee5085e3217c0
TheBusyBiscuit <mrCookieSlime@gmail.com>
2020-10-21 22:31:43 +0200

    Added annotations to exceptions

--- src/main/java/com/gmail/nossr50/api/exceptions/IncompleteNamespacedKeyRegister.java
+++ src/main/java/com/gmail/nossr50/api/exceptions/IncompleteNamespacedKeyRegister.java
@@ -1,9 +1,11 @@
 package com.gmail.nossr50.api.exceptions;
 
+import javax.annotation.Nonnull;
+
 public class IncompleteNamespacedKeyRegister extends RuntimeException {
     private static final long serialVersionUID = -6905157273569301219L;
 
-    public IncompleteNamespacedKeyRegister(String message) {
+    public IncompleteNamespacedKeyRegister(@Nonnull String message) {
         super(message);
     }
 }
--- src/main/java/com/gmail/nossr50/api/exceptions/McMMOPlayerNotFoundException.java
+++ src/main/java/com/gmail/nossr50/api/exceptions/McMMOPlayerNotFoundException.java
@@ -1,11 +1,13 @@
 package com.gmail.nossr50.api.exceptions;
 
+import javax.annotation.Nonnull;
+
 import org.bukkit.entity.Player;
 
 public class McMMOPlayerNotFoundException extends RuntimeException {
     private static final long serialVersionUID = 761917904993202836L;
 
-    public McMMOPlayerNotFoundException(Player player) {
+    public McMMOPlayerNotFoundException(@Nonnull Player player) {
         super("McMMOPlayer object was not found for [NOTE: This can mean the profile is not loaded yet!] : " + player.getName() + " " + player.getUniqueId());
     }
 }
