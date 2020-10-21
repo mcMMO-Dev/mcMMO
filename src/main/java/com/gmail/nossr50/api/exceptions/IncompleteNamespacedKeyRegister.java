0371f00f4bcc7bd8056fad63a5e41aa9413b640f
TheBusyBiscuit <mrCookieSlime@gmail.com>
2020-10-21 22:31:02 +0200

    Generate a serial version uid

--- src/main/java/com/gmail/nossr50/api/exceptions/IncompleteNamespacedKeyRegister.java
+++ src/main/java/com/gmail/nossr50/api/exceptions/IncompleteNamespacedKeyRegister.java
@@ -1,6 +1,8 @@
 package com.gmail.nossr50.api.exceptions;
 
 public class IncompleteNamespacedKeyRegister extends RuntimeException {
+    private static final long serialVersionUID = -6905157273569301219L;
+
     public IncompleteNamespacedKeyRegister(String message) {
         super(message);
     }
