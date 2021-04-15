4f5f3aff8078cef9ad156065dab428278342a386
nossr50 <nossr50@gmail.com>
2021-04-15 10:43:48 -0700

    Oops

--- src/main/java/com/gmail/nossr50/util/BlockUtils.java
+++ src/main/java/com/gmail/nossr50/util/BlockUtils.java
@@ -292,7 +292,7 @@ public final class BlockUtils {
     public static boolean isWithinWorldBounds(@NotNull WorldCompatibilityLayer worldCompatibilityLayer, @NotNull Block block) {
         World world = block.getWorld();
 
-        return block.getY() > worldCompatibilityLayer.getMinWorldHeight(world) || block.getY() < worldCompatibilityLayer.getMaxWorldHeight(world);
+        return block.getY() > worldCompatibilityLayer.getMinWorldHeight(world) && block.getY() < worldCompatibilityLayer.getMaxWorldHeight(world);
     }
 
 }
