989f7fd51b9908f2692dd2090126beacffc74779
nossr50 <nossr50@gmail.com>
2021-04-23 10:45:19 -0700

    Fix Serrated Strikes not processing Rupture for additional targets

--- src/main/java/com/gmail/nossr50/util/skills/CombatUtils.java
+++ src/main/java/com/gmail/nossr50/util/skills/CombatUtils.java
@@ -712,7 +712,7 @@ public final class CombatUtils {
                         NotificationManager.sendPlayerInformation((Player)entity, NotificationType.SUBSKILL_MESSAGE, "Swords.Combat.SS.Struck");
                     }
 
-                    UserManager.getPlayer(attacker).getSwordsManager().processRupture(target);
+                    UserManager.getPlayer(attacker).getSwordsManager().processRupture(livingEntity);
                     break;
 
                 case AXES:
