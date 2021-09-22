162c605dacb52ddd2fae6797ffd7cbd9cb4bb84e
PikaMug <2267126+PikaMug@users.noreply.github.com>
2021-09-22 18:05:41 -0400

    Trigger change event for party create/disband (#4620)

--- src/main/java/com/gmail/nossr50/commands/party/PartyDisbandCommand.java
+++ src/main/java/com/gmail/nossr50/commands/party/PartyDisbandCommand.java
@@ -1,6 +1,7 @@
 package com.gmail.nossr50.commands.party;
 
 import com.gmail.nossr50.datatypes.party.Party;
+import com.gmail.nossr50.datatypes.player.McMMOPlayer;
 import com.gmail.nossr50.events.party.McMMOPartyChangeEvent.EventReason;
 import com.gmail.nossr50.locale.LocaleLoader;
 import com.gmail.nossr50.party.PartyManager;
@@ -15,13 +16,14 @@ public class PartyDisbandCommand implements CommandExecutor {
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, String[] args) {
         if (args.length == 1) {
-            if (UserManager.getPlayer((Player) sender) == null) {
+            final McMMOPlayer mcMMOPlayer = UserManager.getPlayer((Player) sender);
+            if (mcMMOPlayer == null) {
                 sender.sendMessage(LocaleLoader.getString("Profile.PendingLoad"));
                 return true;
             }
 
-            Party playerParty = UserManager.getPlayer((Player) sender).getParty();
-            String partyName = playerParty.getName();
+            final Party playerParty = mcMMOPlayer.getParty();
+            final String partyName = playerParty.getName();
 
             for (Player member : playerParty.getOnlineMembers()) {
                 if (!PartyManager.handlePartyChangeEvent(member, partyName, null, EventReason.KICKED_FROM_PARTY)) {
@@ -31,7 +33,7 @@ public class PartyDisbandCommand implements CommandExecutor {
                 member.sendMessage(LocaleLoader.getString("Party.Disband"));
             }
 
-            PartyManager.disbandParty(playerParty);
+            PartyManager.disbandParty(mcMMOPlayer, playerParty);
             return true;
         }
         sender.sendMessage(LocaleLoader.getString("Commands.Usage.1", "party", "disband"));
