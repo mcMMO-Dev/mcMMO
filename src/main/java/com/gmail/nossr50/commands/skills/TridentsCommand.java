9cbe10827f1d55aae72eb5fc00e9e5e5665a050b
nossr50 <nossr50@gmail.com>
2024-01-14 09:27:04 -0800

    Buff end game skills

--- src/main/java/com/gmail/nossr50/commands/skills/TridentsCommand.java
+++ src/main/java/com/gmail/nossr50/commands/skills/TridentsCommand.java
@@ -1,15 +1,21 @@
 package com.gmail.nossr50.commands.skills;
 
+import com.gmail.nossr50.datatypes.player.McMMOPlayer;
 import com.gmail.nossr50.datatypes.skills.PrimarySkillType;
 import com.gmail.nossr50.datatypes.skills.SubSkillType;
+import com.gmail.nossr50.util.player.UserManager;
 import com.gmail.nossr50.util.skills.CombatUtils;
 import com.gmail.nossr50.util.skills.SkillUtils;
 import com.gmail.nossr50.util.text.TextComponentFactory;
 import net.kyori.adventure.text.Component;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Cat;
 import org.bukkit.entity.Player;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import static com.gmail.nossr50.datatypes.skills.SubSkillType.*;
 
 public class TridentsCommand extends SkillCommand {
 
@@ -27,15 +33,27 @@ public class TridentsCommand extends SkillCommand {
     @Override
     protected List<String> statsDisplay(Player player, float skillValue, boolean hasEndurance, boolean isLucky) {
         List<String> messages = new ArrayList<>();
-
-        if (SkillUtils.canUseSubskill(player, SubSkillType.TRIDENTS_SUPER)) {
-            messages.add("Tridents Super Ability");
-            //TODO: Implement Tridents Super
+        McMMOPlayer mmoPlayer = UserManager.getPlayer(player);
+        if (mmoPlayer == null) {
+            return messages;
         }
 
-        if(SkillUtils.canUseSubskill(player, SubSkillType.TRIDENTS_TRIDENTS_LIMIT_BREAK)) {
-            messages.add(getStatMessage(SubSkillType.TRIDENTS_TRIDENTS_LIMIT_BREAK,
-                    String.valueOf(CombatUtils.getLimitBreakDamageAgainstQuality(player, SubSkillType.TRIDENTS_TRIDENTS_LIMIT_BREAK, 1000))));
+        messages.add(ChatColor.DARK_AQUA + "Reminder: " + ChatColor.GOLD + "This is a BETA version of mcMMO, please report any bugs you find!");
+        messages.add(ChatColor.GOLD + "Tridents is a " + ChatColor.RED + "WIP" +ChatColor.GOLD + " skill that is still being developed, please leave feedback in our discord!");
+
+//        if (SkillUtils.canUseSubskill(player, TRIDENTS_SUPER)) {
+//            messages.add("Tridents Super Ability");
+//            //TODO: Implement Tridents Super
+//        }
+
+        if(SkillUtils.canUseSubskill(player, TRIDENTS_TRIDENTS_LIMIT_BREAK)) {
+            messages.add(getStatMessage(TRIDENTS_TRIDENTS_LIMIT_BREAK,
+                    String.valueOf(CombatUtils.getLimitBreakDamageAgainstQuality(player, TRIDENTS_TRIDENTS_LIMIT_BREAK, 1000))));
+        }
+
+        if(SkillUtils.canUseSubskill(player, TRIDENTS_IMPALE)) {
+            messages.add(getStatMessage(TRIDENTS_IMPALE,
+                    String.valueOf(mmoPlayer.getTridentsManager().impaleDamageBonus())));
         }
 
         return messages;
