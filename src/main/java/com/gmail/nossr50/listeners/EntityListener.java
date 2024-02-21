33c467e7cec44546e3f39d3918112159610f56e2
Dieu <info@l4b.org>
2024-02-21 19:20:34 +0100

    Only run EntityListener#checkParties on EntityCombustByEntityEvent if Parties are enabled

--- src/main/java/com/gmail/nossr50/listeners/EntityListener.java
+++ src/main/java/com/gmail/nossr50/listeners/EntityListener.java
@@ -259,7 +259,7 @@ public class EntityListener implements Listener {
     @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
     public void onEntityCombustByEntityEvent(EntityCombustByEntityEvent event) {
         //Prevent players from setting fire to each other if they are in the same party
-        if(event.getEntity() instanceof Player defender) {
+        if(mcMMO.p.getPartyConfig().isPartyEnabled() && event.getEntity() instanceof Player defender) {
 
             if(event.getCombuster() instanceof Projectile projectile) {
                 if(projectile.getShooter() instanceof Player attacker) {
