080d407c142595941aed4f6d76b864e4a968b6be
nossr50 <nossr50@gmail.com>
2026-01-27 13:03:50 -0800

    another fix for compatibility with Excellent Enchants

--- src/main/java/com/gmail/nossr50/listeners/BlockListener.java
+++ src/main/java/com/gmail/nossr50/listeners/BlockListener.java
@@ -242,11 +242,16 @@ public class BlockListener implements Listener {
             return;
         }
 
-        BlockState blockState = event.getNewState();
+        final BlockState newState = event.getNewState();
+
+        if (!newState.isPlaced()) {
+            // not backed by a real block
+            return;
+        }
 
         if (ExperienceConfig.getInstance().isSnowExploitPrevented() && BlockUtils.shouldBeWatched(
-                blockState)) {
-            Block block = blockState.getBlock();
+                newState)) {
+            final Block block = newState.getBlock();
 
             if (BlockUtils.isWithinWorldBounds(block)) {
                 BlockUtils.setUnnaturalBlock(block);
